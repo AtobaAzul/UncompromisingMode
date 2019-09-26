1c3666251d51b62c8ef1e08a2787eb3a9f9e116d
Ionut Lupuleasa <ionut.lupuleasa@hpe.com>
2019-09-26 11:30:59 +0300

    Changed applied horticulture to use leafy meat instead of seed

--- init/init_recipes.lua
+++ init/init_recipes.lua
@@ -1,11 +1,15 @@
 --	[ 			Requised stuff			]	--
-	
+-- The global objects needed for recipe changes
+-- Find the default recipes in recipes.lua
 	TECH = GLOBAL.TECH
 	Recipe = GLOBAL.Recipe
 	RECIPETABS = GLOBAL.RECIPETABS
 	Ingredient = GLOBAL.Ingredient
 	AllRecipes = GLOBAL.AllRecipes
-	
+	STRINGS = GLOBAL.STRINGS
+	TECH = GLOBAL.TECH
+	CUSTOM_RECIPETABS = GLOBAL.CUSTOM_RECIPETABS
+
 --	[ 				Recipes				]	--
 	
 --	Tool
@@ -31,5 +35,17 @@
 --	Ancient
 	
 --	Celestial
+
+-- Wicker Books
+-- Leafy meat cost for applied horticulture
+if GetModConfigData("harder_recipes") then
+	-- Also use fertilizer instead of poop
+	Recipe("book_gardening", {Ingredient("papyrus", 2), Ingredient("plantmeat", 1), Ingredient("fertilizer", 1)}, CUSTOM_RECIPETABS.BOOKS, TECH.SCIENCE_ONE, nil, nil, nil, nil, "bookbuilder")
+else
+	-- Use poop as normal
+	Recipe("book_gardening", {Ingredient("papyrus", 2), Ingredient("plantmeat", 1), Ingredient("poop", 1)}, CUSTOM_RECIPETABS.BOOKS, TECH.SCIENCE_ONE, nil, nil, nil, nil, "bookbuilder")
+end
+
+
 	
 --	Configuration Recipes
