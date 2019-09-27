98c7e0db7e02f75f4520b9a02aa8b258d9d0827d
Ionut Lupuleasa <ionut.lupuleasa@hpe.com>
2019-09-27 14:18:58 +0300

    Cleaned up livingtree buffs

--- init/init_treebuffs.lua
+++ init/init_treebuffs.lua
@@ -10,60 +10,52 @@ Assets = {
 
 local GLOBAL.TUNING.LIVINGTREE_ROOT_ATTACK_POSITION_VARIANCE = 30;
 local GLOBAL.TUNING.LIVINGTREE_ROOT_TARGET_DIST = GLOBAL.TUNING.DECID_MONSTER_TARGET_DIST;
-AddPrefabPostInit("livingtree", function (inst)
 
-    local function distsq(v1, v2, v3, v4)
-
-        if v4 and v3 and v2 and v1 then
-            local dx = v1-v3
-            local dy = v2-v4
-            return dx*dx + dy*dy
+--Copied from DeciduousTreeUpdater:OnUpdate in deciduoustreeupdater.lua
+local function spawn_root_attack(inst,chopper)
+    if chopper ~= nil then
+        local root = GLOBAL.SpawnPrefab("deciduous_root")
+        local targdist = GLOBAL.TUNING.LIVINGTREE_ROOT_TARGET_DIST
+        local x, y, z = chopper.Transform:GetWorldPosition()
+        local mx, my, mz = inst.Transform:GetWorldPosition()
+        local mdistsq = inst:GetDistanceSqToInst(chopper)
+        local targdistsq = targdist * targdist
+        local possition_variance_x = (GLOBAL.math.random() - 0.5) * TUNING.LIVINGTREE_ROOT_ATTACK_POSITION_VARIANCE;
+        local possition_variance_z = (GLOBAL.math.random() - 0.5) * TUNING.LIVINGTREE_ROOT_ATTACK_POSITION_VARIANCE;
+        local rootpos = GLOBAL.Vector3(mx + possition_variance_x, 0, mz + possition_variance_z)
+        local angle = inst:GetAngleToPoint(rootpos) * GLOBAL.DEGREES
+        if mdistsq > targdistsq then
+            rootpos.x = x + GLOBAL.math.cos(angle) * targdist
+            rootpos.z = z - GLOBAL.math.sin(angle) * targdist
         end
+
+        root.AnimState:SetBuild("tree_leaf_spike_lt")
+        root.AnimState:SetBank("tree_leaf_spike_lt")
+        root.Transform:SetPosition(x + 1.75 * math.cos(angle), 0, z - 1.75 * math.sin(angle))
+        root:PushEvent("givetarget", { target = chopper, targetpos = rootpos, targetangle = angle, owner = inst })                      
     end
+end
 
-    --Copied from DeciduousTreeUpdater:OnUpdate in deciduoustreeupdater.lua
-    local function spawn_root_attack(inst,chopper)
-        if chopper ~= nil then
-            local root = GLOBAL.SpawnPrefab("deciduous_root")
-            local targdist = GLOBAL.TUNING.LIVINGTREE_ROOT_TARGET_DIST
-            local x, y, z = chopper.Transform:GetWorldPosition()
-            local mx, my, mz = inst.Transform:GetWorldPosition()
-            local mdistsq = distsq(x, z, mx, mz)
-            local targdistsq = targdist * targdist
-            local possition_variance_x = (GLOBAL.math.random() - 0.5) * TUNING.LIVINGTREE_ROOT_ATTACK_POSITION_VARIANCE;
-            local possition_variance_z = (GLOBAL.math.random() - 0.5) * TUNING.LIVINGTREE_ROOT_ATTACK_POSITION_VARIANCE;
-            local rootpos = GLOBAL.Vector3(mx + possition_variance_x, 0, mz + possition_variance_z)
-            local angle = inst:GetAngleToPoint(rootpos) * GLOBAL.DEGREES
-            if mdistsq > targdistsq then
-                rootpos.x = x + GLOBAL.math.cos(angle) * targdist
-                rootpos.z = z - GLOBAL.math.sin(angle) * targdist
-            end
+-- Override onworked
+local function onworked(inst, chopper, workleft)
+    if not (chopper ~= nil and chopper:HasTag("playerghost")) then
+        inst.SoundEmitter:PlaySound(
+            chopper ~= nil and chopper:HasTag("beaver") and
+            "dontstarve/characters/woodie/beaver_chop_tree" or
+            "dontstarve/wilson/use_axe_tree"
+        )
 
-            root.AnimState:SetBuild("tree_leaf_spike_lt")
-            root.AnimState:SetBank("tree_leaf_spike_lt")
-            root.Transform:SetPosition(x + 1.75 * math.cos(angle), 0, z - 1.75 * math.sin(angle))
-            root:PushEvent("givetarget", { target = chopper, targetpos = rootpos, targetangle = angle, owner = inst })                      
-        end
+        -- Spawn deciduous_root
+        spawn_root_attack(inst,chopper)
     end
+    inst.SoundEmitter:PlaySound("dontstarve/creatures/leif/livingtree_hit")
+    inst.AnimState:PlayAnimation("chop")
+    inst.AnimState:PushAnimation("idle", true)
+end
 
-    -- Override onworked
-    local function onworked(inst, chopper, workleft)
-        if not (chopper ~= nil and chopper:HasTag("playerghost")) then
-            inst.SoundEmitter:PlaySound(
-                chopper ~= nil and chopper:HasTag("beaver") and
-                "dontstarve/characters/woodie/beaver_chop_tree" or
-                "dontstarve/wilson/use_axe_tree"
-            )
-
-            -- Spawn deciduous_root
-            spawn_root_attack(inst,chopper)
-        end
-        inst.SoundEmitter:PlaySound("dontstarve/creatures/leif/livingtree_hit")
-        inst.AnimState:PlayAnimation("chop")
-        inst.AnimState:PushAnimation("idle", true)
-    end
-
+local function livingtree_postinit(inst)
     -- Change the onworked callback to the above
     inst.components.workable:SetOnWorkCallback(onworked)
+end
 
-end)            
+AddPrefabPostInit("livingtree", livingtree_postinit)
