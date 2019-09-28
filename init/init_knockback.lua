63a02b3d39e2e243118f870bcd385faf8a905955
Hornet <alsaadiomar74@gmail.com>
2019-09-27 21:49:09 -0400

    Added different powers for mobs, (moose YEETS you)

--- init/init_knockback.lua
+++ init/init_knockback.lua
@@ -3,26 +3,37 @@ local GIANTS =
 	["bearger"] =
     {
         radius = 125,
+		power = 1.50
     },
 	["dragonfly"] =
     {
         radius = 75,
+		power = 1.25
     },
 	["leif"] =
     {
         radius = 75,
+		power = 1.25
     },
 	["minotaur"] =
     {
         radius = 200,
+		power = 1.75
     },
 	["rook"] =
     {
         radius = 150,
+		power = 1.5
     },
 	["krampus"] =
     {
         radius = 150,
+		power = 1.50
+    },
+	["moose"] =
+    {
+        radius = 200,
+		power = 2
     },
 }
 
@@ -30,7 +41,7 @@ for k, v in pairs(GIANTS) do
 	AddPrefabPostInit(k, function(inst)
 		local function OnHitOther(inst, other)
 			if other ~= nil then
-				other:PushEvent("knockback", {knocker = inst, radius = v.radius})
+				other:PushEvent("knockback", {knocker = inst, radius = v.radius, strengthmult = v.power})
 			end
 		end
 	
