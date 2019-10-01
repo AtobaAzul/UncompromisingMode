fef66a75409d62190d675ebef83f2198958d4e4a
Ionut Lupuleasa <ionut.lupuleasa@hpe.com>
2019-10-01 11:34:04 +0300

    Meatballs hunger set to 50 (down from 62.5)

---
+++ init/init_crockpot.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------
+-- Crockpot food changes
+-- Relevant: preparedfods.lua, foods (variable)
+-----------------------------------------------------------------
+
+-- 3 ice wet goop
+--TODO
+
+-- 3 twigs wet goop
+--TODO
+
+-- meatballs is 50 hunger
+foods.meatballs.hunger = TUNING.CALORIES_SMALL*4
