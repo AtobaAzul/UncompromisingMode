a76c27b82ccb063edac1a725a3ac21f0b58394c2
Ionut Lupuleasa <ionut.lupuleasa@hpe.com>
2019-10-03 14:51:49 +0300

    Woodie needs presthatilator for kitschy idols

--- init/init_character_changes/woodie.lua
+++ init/init_character_changes/woodie.lua
@@ -1,3 +1,17 @@
+-- Change woodie idol recipe to require presthatilator
+TECH = GLOBAL.TECH
+Recipe = GLOBAL.Recipe
+RECIPETABS = GLOBAL.RECIPETABS
+Ingredient = GLOBAL.Ingredient
+AllRecipes = GLOBAL.AllRecipes
+STRINGS = GLOBAL.STRINGS
+TECH = GLOBAL.TECH
+CUSTOM_RECIPETABS = GLOBAL.CUSTOM_RECIPETABS
+
+Recipe("wereitem_goose", {Ingredient("monstermeat", 3), Ingredient("seeds", 3)}, RECIPETABS.MAGIC, TECH.MAGIC_TWO, nil, nil, nil, nil, "werehuman")
+Recipe("wereitem_beaver", {Ingredient("monstermeat", 3), Ingredient("log", 2)}, RECIPETABS.MAGIC, TECH.MAGIC_TWO, nil, nil, nil, nil, "werehuman")
+Recipe("wereitem_moose", {Ingredient("monstermeat", 3), Ingredient("cutgrass", 2)}, RECIPETABS.MAGIC, TECH.MAGIC_TWO, nil, nil, nil, nil, "werehuman")
+
 -- If goose is over water, increase wetness
 AddPrefabPostInit("woodie", function (inst)
     
