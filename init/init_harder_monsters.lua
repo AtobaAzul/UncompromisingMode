283babfd39710411af38be1ab2ee6d3c5717273a
Ionut Lupuleasa <ionut.lupuleasa@hpe.com>
2019-10-03 15:48:56 +0300

    Pigs attack those that destroy their houses

--- init/init_harder_monsters.lua
+++ init/init_harder_monsters.lua
@@ -12,3 +12,25 @@ AddPrefabPostInit("batcave", function (inst)
     inst.components.childspawner:SetMaxChildren(TUNING.BATCAVE_MAX_CHILDREN*GLOBAL.TUNING.DSTU.MONSTER_BAT_CAVE_NR_INCREASE)
     --inst.components.childspawner.childname = "bat"
 end)
+
+--Pigs defend their turf if their home is destroyed
+local function onhammered(inst, worker)
+    if inst.components.burnable ~= nil and inst.components.burnable:IsBurning() then
+        inst.components.burnable:Extinguish()
+    end
+    if inst.doortask ~= nil then
+        inst.doortask:Cancel()
+        inst.doortask = nil
+    end
+    if inst.components.spawner ~= nil and inst.components.spawner:IsOccupied() then
+        inst.components.spawner:ReleaseChild()
+        inst.components.spawner.child.combat:SetTarget(worker) --<< Get the pig to attack the perpetrator of the crime against pig-kind
+    end
+    inst.components.lootdropper:DropLoot()
+    local fx = SpawnPrefab("collapse_big")
+    fx.Transform:SetPosition(inst.Transform:GetWorldPosition())
+    fx:SetMaterial("wood")
+    inst:Remove()
+end
+
+AddPrefabPostInit("pighouse", onhammered)
