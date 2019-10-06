122f1394f6c7eabf00f116f63d22bad0374e3999
Ionut Lupuleasa <ionut.lupuleasa@hpe.com>
2019-10-06 23:06:05 +0300

    Add tripover chance at 100 wetness

--- init/init_harder_weather.lua
+++ init/init_harder_weather.lua
@@ -14,35 +14,61 @@
 -- Make you take damage randomly when at Max Moisture wetness, by falling and knockbacking
 -- Relevant: moisture.lua, onmaxmoisture
 ----------------------------------------------------------------------------------------------------------
---TODO: get this working
+--TODO: Add slip sound
 
-function trip_over(player)
-    print("tripped over")
+local function trip_damage(player)
+    player.components.health:DoDelta(-GLOBAL.TUNING.DSTU.TRIPOVER_HEALTH_DAMAGE)
+end
+
+local function trip_over(player)
     --trip over you scrubby eel
-    player:PushEvent("knockback", {knocker = player, radius = GLOBAL.TUNING.DSTU.TRIPOVER_KNOCKABCK_RADIUS})
-    player.components.health:DeltaPenalty(GLOBAL.TUNING.DSTU.TRIPOVER_HEALTH_PENALTY)
+    player:PushEvent("knockback", {knocker = player, radius = GLOBAL.TUNING.DSTU.TRIPOVER_KNOCKABCK_RADIUS, strengthmult = 0.5})
+    player:DoTaskInTime(0.5, trip_damage)
+end
+
+local function trip_chance_check()
+    return GLOBAL.math.random() < GLOBAL.TUNING.DSTU.TRIPOVER_ONMAXWET_CHANCE_PER_SEC
 end
 
 local function trip_over_chance_on_maxwet(player)
-    print("fall when wet?")
-    if(player~=nil and player.GetMoisture == self.GetMaxMoisture) then
-        print("totally wet")
-        if (GLOBAL.math.random() > GLOBAL.TUNING.DSTU.TRIPOVER_ONMAXWET_CHANCE)-- and
-            --~player.walking) then
-            -- or try self.components.playercontroller.directwalking
-            -- or try self.status == RUNNING
-            trip_over(player)
+    if player~=nil and (player:GetMoisture() == player:GetMaxMoisture()) and player.sg ~= nil and player.sg:HasStateTag("moving") then
+        local time = GLOBAL.GetTime()
+        if player.components.moisture.lastslip_check < time - 1 then
+            if player.components.moisture.lastslip_time < time - GLOBAL.TUNING.DSTU.TRIPOVER_ONMAXWET_COOLDOWN  then
+                print("chance")
+                if trip_chance_check() then
+                    print("hit")
+                    trip_over(player)
+                    --play sound too
+                    player.SoundEmitter:PlaySound("dontstarve/common/dropGeneric")
+                    player.components.moisture.lastslip_time = time
+                end
+            end
+            print("check")
+            player.components.moisture.lastslip_check = time
         end
     end
 end
 
-AddComponentPostInit("moisture", function(self)
-    function self:LongUpdate(dt)
-            self:OnUpdate(dt)
-            print("moisture long update")
-            trip_over_chance_on_maxwet(self.inst.player_classified)
+local function OnUpdate(self, dt)
+    print(dt)
+    if self ~= nil and self.inst ~= nil then
+        local _OldOnUpdate = self.OnUpdate
+        
+        self.OnUpdate = function(self, dt)
+            _OldOnUpdate(self, dt)
+            trip_over_chance_on_maxwet(self.inst)
+        end
+    end
+end
+
+AddComponentPostInit("moisture", function(component)
+    if component ~= nil and GLOBAL ~= nil then
+        component.lastslip_time = GLOBAL.GetTime()
+        component.lastslip_check = GLOBAL.GetTime()
     end
 end)
+AddComponentPostInit("moisture", OnUpdate)
 
 ----------------------------------------------------------------------------------------------------------
 -- Increase the chance of dropping wet tool on hit
