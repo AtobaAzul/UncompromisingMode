1f9d0ec3285f4875e2755d8d3ee6fbe90dc62a2c
Zarklord <Bestgoaltenderever@gmail.com>
2019-10-21 09:12:23 -0400

    implemented a new post init system, please review.

---
+++ postinit/prefabs/cave.lua
@@ -0,0 +1,10 @@
+local env = env
+GLOBAL.setfenv(1, GLOBAL)
+-----------------------------------------------------------------
+env.AddPrefabPostInit("cave", function(inst)
+    if not TheWorld.ismastersim then
+        return
+    end
+    
+    inst:AddComponent("cavedeerclopsspawner")
+end)
