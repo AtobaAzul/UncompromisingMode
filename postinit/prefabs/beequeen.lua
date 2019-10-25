d9814b12c49a0cb186665d219d1721e5a60667f6
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2019-10-25 18:53:43 -0500

    Update beequeen.lua

--- postinit/prefabs/beequeen.lua
+++ postinit/prefabs/beequeen.lua
@@ -14,14 +14,3 @@ env.AddPrefabPostInit("beequeen", function(inst)
         inst.components.combat:SetAreaDamage(TUNING.DEERCLOPS_AOE_RANGE/2, TUNING.DEERCLOPS_AOE_SCALE, isnotbee) -- you can edit these values to your liking -Axe
     end                                     
 end)
---Treeguard now has AOE - Axe
-AddPrefabPostInit("leif", function (inst)
-    if inst ~= nil and inst.components ~= nil and inst.components.combat ~= nil then
-		local function isnottree(ent)
-			if ent ~= nil and not ent:HasTag("leif") then -- fix to friendly AOE: refer for later AOE mobs -Axe
-				return true
-			end
-		end
-        inst.components.combat:SetAreaDamage(TUNING.DEERCLOPS_AOE_RANGE/2, TUNING.DEERCLOPS_AOE_SCALE, isnottree) -- you can edit these values to your liking -Axe
-    end                                     
-end)
