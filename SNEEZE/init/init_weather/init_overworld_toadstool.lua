a5a52c79b640807673a0ed19bcc645a8bd2e986f
Scrimbles <40772985+Shodance@users.noreply.github.com>
2019-12-21 23:43:39 -0500

    oops accidently put an extra copy of the mod inside the mod

--- SNEEZE/init/init_weather/init_overworld_toadstool.lua
+++
@@ -1,8 +0,0 @@
-AddPrefabPostInit("forest_network",function()
-	-- TODO check that we are in forest
-	if GLOBAL.TheWorld.ismastersim then
-		if not GLOBAL.TheWorld.components.toadstoolspawner then
-			GLOBAL.TheWorld:AddComponent("toadstoolspawner")
-		end
-	end
-end)
