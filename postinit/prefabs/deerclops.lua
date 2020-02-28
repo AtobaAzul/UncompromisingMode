338dd4be44a69f7c0f6c5038a83977fd4a94b6b0
Scrimbles <40772985+Shodance@users.noreply.github.com>
2020-02-28 00:18:54 -0500

    Adding a light made certain faces go invisible, I HAVE NO IDEA WHY

--- postinit/prefabs/deerclops.lua
+++ postinit/prefabs/deerclops.lua
@@ -1,23 +1,20 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------
+
+local PHASE2_HEALTH = .5
+
+local function OnNewState(inst, data)
+    if not (inst.sg:HasStateTag("sleeping") or inst.sg:HasStateTag("waking")) then
+        inst.Light:SetIntensity(.6)
+        inst.Light:SetRadius(8)
+        inst.Light:SetFalloff(3)
+        inst.Light:SetColour(1, 0, 0)
+    end
+end
+
 local function EnterPhase2Trigger(inst)
     if not inst.components.health:IsDead() then
-	
-		inst.sg:GoToState("taunt")
-
-			if not IsSpecialEventActive(SPECIAL_EVENTS.WINTERS_FEAST) then
-				inst.AnimState:SetBuild("deerclops_yule")
-
-				inst.entity:AddLight()
-				inst.Light:SetIntensity(.6)
-				inst.Light:SetRadius(8)
-				inst.Light:SetFalloff(3)
-				inst.Light:SetColour(1, 0, 0)
-				
-				inst:AddComponent("timer")
-			end
-		
 		local upgradeburst = SpawnPrefab("maxwell_smoke")
 		upgradeburst.Transform:SetPosition(inst.Transform:GetWorldPosition())
 		upgradeburst.Transform:SetScale(3, 3, 3)
@@ -26,7 +23,25 @@ local function EnterPhase2Trigger(inst)
 		inst.components.combat:SetRange(TUNING.DEERCLOPS_ATTACK_RANGE * 1.1)
 		inst.components.combat:SetAreaDamage(TUNING.DEERCLOPS_AOE_RANGE * 1.1, TUNING.DEERCLOPS_AOE_SCALE * 1.1)
 		inst.components.combat:SetAttackPeriod(TUNING.DEERCLOPS_ATTACK_PERIOD * 0.9)
-	
+		
+		
+
+			if not IsSpecialEventActive(SPECIAL_EVENTS.WINTERS_FEAST) then
+				inst.AnimState:SetBuild("deerclops_yule")
+--[[
+				inst.entity:AddLight()
+				inst.Light:SetIntensity(.6)
+				inst.Light:SetRadius(8)
+				inst.Light:SetFalloff(3)
+				inst.Light:SetColour(1, 0, 0)
+--]]
+				inst:AddComponent("timer")
+				--inst:ListenForEvent("newstate", OnNewState)
+				
+			end
+		
+		
+		inst.sg:GoToState("taunt")
 		inst.enraged = true
 	
 	end
@@ -52,8 +67,6 @@ env.AddPrefabPostInit("deerclops", function(inst)
 	if not TheWorld.ismastersim then
 		return
 	end
-	
-	local PHASE2_HEALTH = .5
 	
 	inst.OnSave = OnSave
     inst.OnPreLoad = OnPreLoad
