7b670c21737c164accc65b603dc28986ba6bf12c
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2020-05-03 08:29:19 -0500

    Trapdoors are no longer infested with bees

--- scripts/prefabs/trapdoor.lua
+++ scripts/prefabs/trapdoor.lua
@@ -5,49 +5,20 @@ local prefabs =
 
 local assets =
 {
-    Asset("ANIM", "anim/wasphive.zip"),
-    Asset("SOUND", "sound/bee.fsb"), --replace with wasp
 }
 
 
 
---local function OnKilled(inst)
---    inst:RemoveComponent("childspawner")
---    inst.AnimState:PlayAnimation("cocoon_dead", true)
---    RemovePhysicsColliders(inst)
---
---    inst.SoundEmitter:KillSound("loop")
---
---    inst.SoundEmitter:PlaySound("dontstarve/bee/beehive_destroy") --replace with wasp
---    inst.components.lootdropper:DropLoot(inst:GetPosition()) --any loot drops?
---end
+
 
 local function onnear(inst, target)
-    --hive pop open? Maybe rustle to indicate danger?
-    --more and more come out the closer you get to the nest?
     if inst.components.childspawner ~= nil and not target:HasTag("monster") then
         inst.components.childspawner:ReleaseAllChildren(target, "spider_trapdoor")
     end
 end
 
---local function onhitbyplayer(inst, attacker, damage)
---    if inst.components.childspawner ~= nil then
---        inst.components.childspawner:ReleaseAllChildren(attacker, "spider_trapdoor")
---    end
---    if not inst.components.health:IsDead() and then
---        inst.SoundEmitter:PlaySound("dontstarve/bee/beehive_hit")
---        inst.AnimState:PlayAnimation("cocoon_small_hit")
---        inst.AnimState:PushAnimation("cocoon_small", true)
---    end
---end
 
-local function OnEntityWake(inst)
-    inst.SoundEmitter:PlaySound("dontstarve/bee/killerbee_hive_LP", "loop")
-end
 
-local function OnEntitySleep(inst)
-    inst.SoundEmitter:KillSound("loop")
-end
 
 local function OnHaunt(inst)
     if inst.components.childspawner == nil or
@@ -196,8 +167,6 @@ local function fn()
     MakeSnowCovered(inst)
     -------------------------
     inst:AddComponent("inspectable")
-    inst.OnEntitySleep = OnEntitySleep
-    inst.OnEntityWake = OnEntityWake
 
     inst:AddComponent("hauntable")
     inst.components.hauntable:SetHauntValue(TUNING.HAUNT_MEDIUM)
