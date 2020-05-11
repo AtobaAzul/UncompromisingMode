2e9809b9549f2c7d1a81ef9458af2c59761013bb
Scrimbles <40772985+Shodance@users.noreply.github.com>
2020-05-10 21:11:01 -0400

    Removed some action related stuff in the mod main, could possibly be causing problems when combined with other mods? Not entirely sure...

--- modmain.lua
+++ modmain.lua
@@ -1,48 +1,4 @@
---[[
-	Quick ref for global variables
-local _G = GLOBAL
-local require = _G.require
-local TUNING = _G.TUNING
-local Vector3 = _G.Vector3
-local ACTIONS = _G.ACTIONS
-local RECIPETABS = _G.RECIPETABS
-local STRINGS = _G.STRINGS
-local TECH = _G.TECH
-local GROUND = _G.GROUND
-local SpawnPrefab = _G.SpawnPrefab
-local getlocal = _G.debug.getlocal
-local getupvalue = _G.debug.getupvalue
-local EQUIPSLOTS = _G.EQUIPSLOTS
-]]
 local require = GLOBAL.require
-local ACTIONS = GLOBAL.ACTIONS
-
-
-local BLOWNOSE = GLOBAL.Action({ priority=1})
-BLOWNOSE.str = "Blow Nose"
-BLOWNOSE.id = "BLOWNOSE"
-
-BLOWNOSE.fn = function(act)
-    if act.invobject ~= nil and act.doer ~= nil then
-        return act.invobject.components.blow_nose:Play(act.doer)
-    end
-end
-
-AddAction(BLOWNOSE)
-
-
-
-
-AddComponentAction("INVENTORY", "blow_nose", function(inst, doer, actions)
-    --if right then
-        if doer ~= nil then
-            table.insert(actions, GLOBAL.ACTIONS.BLOWNOSE)
-        end
-    --end
-end)
-
-
-
 
 PrefabFiles = require("uncompromising_prefabs")
 
