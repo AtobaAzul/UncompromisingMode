f69f71fa3edd80851102c61313ca37ed257f6401
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2020-05-21 18:25:58 -0500

    trapdoor and trapdoor spider work

--- scripts/prefabs/trapdoor.lua
+++ scripts/prefabs/trapdoor.lua
@@ -17,8 +17,12 @@ SetSharedLootTable( 'trapdoor',
 
 
 local function onnear(inst, target)
-    if inst.components.childspawner ~= nil and not target:HasTag("monster") then
+    if inst.components.childspawner ~= nil  then
+		if target:HasTag("spiderwhisperer") or target:HasTag("spiderdisguise") then
+		inst.components.childspawner:ReleaseAllChildren(nil, "spider_trapdoor")
+		else
         inst.components.childspawner:ReleaseAllChildren(target, "spider_trapdoor")
+		end
     end
 end
 
