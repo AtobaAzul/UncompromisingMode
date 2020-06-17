f5bf0b3265eaf407d294d194686102478bf26430
Scrimbles <40772985+Shodance@users.noreply.github.com>
2020-06-16 21:23:47 -0400

    Frogs will no longer get stuck walking towards out of reach food (aka, on water or boats)

--- postinit/brains/frogbrain.lua
+++ postinit/brains/frogbrain.lua
@@ -3,12 +3,28 @@ GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------
 local SEE_DIST = 20
 
+local FINDFOOD_CANT_TAGS = { "outofreach" }
 local function EatFoodAction(inst)
 	if inst.sg.currentstate.name == "fall" then
 		return nil
 	end
-    local target = FindEntity(inst, SEE_DIST, function(item) return inst.components.eater:CanEat(item) and item:IsOnPassablePoint(true) end)
+    --[[local target = FindEntity(inst, SEE_DIST, function(item) return inst.components.eater:CanEat(item) and item:IsOnPassablePoint(true) end)
     return target ~= nil and BufferedAction(inst, target, ACTIONS.EAT) or nil
+	]]
+	 local target = FindEntity(inst,
+        SEE_DIST,
+        function(item)
+            return item.prefab ~= "mandrake"
+                and item.components.edible ~= nil
+                and item:IsOnPassablePoint()
+                and inst.components.eater:CanEat(item)
+        end,
+        nil,
+        FINDFOOD_CANT_TAGS
+    )
+    if target ~= nil then
+        return BufferedAction(inst, target, ACTIONS.EAT) or nil
+    end
 end
 
 local function FrogFindFood(self)
