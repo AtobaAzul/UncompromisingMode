d1bde0d0dcc512729ec3a35cd7cd2a6c25264703
Scrimbles <40772985+Shodance@users.noreply.github.com>
2020-08-07 06:56:33 -0400

    Willows lighter becomes properly castable only when shes holding it

--- postinit/prefabs/lighter.lua
+++ postinit/prefabs/lighter.lua
@@ -1,50 +1,6 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------
-local function onequip(inst, owner)
-    inst.components.burnable:Ignite()
-
-    local skin_build = inst:GetSkinBuild()
-    if skin_build ~= nil then
-        owner:PushEvent("equipskinneditem", inst:GetSkinName())
-        owner.AnimState:OverrideItemSkinSymbol("swap_object", skin_build, "swap_lighter", inst.GUID, "swap_lighter")
-    else
-        owner.AnimState:OverrideSymbol("swap_object", "swap_lighter", "swap_lighter")
-    end
-
-    owner.AnimState:Show("ARM_carry")
-    owner.AnimState:Hide("ARM_normal")
-
-    owner.SoundEmitter:PlaySound("dontstarve/wilson/lighter_on")
-
-
-    if inst.fires == nil then
-        inst.fires = {}
-
-        for i, fx_prefab in ipairs(inst:GetSkinName() == nil and { "lighterfire" } or SKIN_FX_PREFAB[inst:GetSkinName()] or {}) do
-            local fx = SpawnPrefab(fx_prefab)
-            fx.entity:SetParent(owner.entity)
-            fx.entity:AddFollower()
-            fx.Follower:FollowSymbol(owner.GUID, "swap_object", fx.fx_offset_x, fx.fx_offset_y, 0)
-            fx:AttachLightTo(owner)
-
-            table.insert(inst.fires, fx)
-        end
-    end
-
-	if owner:HasTag("pyromaniac") then
-		inst.components.fueled:StopConsuming()
-	end
-
-
-    --[[if inst.fire == nil then
-        inst.fire = SpawnPrefab("lighterfire")
-        --inst.fire.Transform:SetScale(.125, .125, .125)
-        inst.fire.entity:AddFollower()
-        inst.fire.Follower:FollowSymbol(owner.GUID, "swap_object", 56, -40, 0)
-    end]]
-end
-
 local function createlight(inst)
 
     local caster = inst.components.inventoryitem.owner
@@ -90,6 +46,57 @@ local function createlight(inst)
 	local reduce = inst.components.fueled:GetPercent() - 0.1001
 	inst.components.fueled:SetPercent(reduce)
 	
+end
+
+local function onequip(inst, owner)
+    inst.components.burnable:Ignite()
+
+    local skin_build = inst:GetSkinBuild()
+    if skin_build ~= nil then
+        owner:PushEvent("equipskinneditem", inst:GetSkinName())
+        owner.AnimState:OverrideItemSkinSymbol("swap_object", skin_build, "swap_lighter", inst.GUID, "swap_lighter")
+    else
+        owner.AnimState:OverrideSymbol("swap_object", "swap_lighter", "swap_lighter")
+    end
+
+    owner.AnimState:Show("ARM_carry")
+    owner.AnimState:Hide("ARM_normal")
+
+    owner.SoundEmitter:PlaySound("dontstarve/wilson/lighter_on")
+
+
+    if inst.fires == nil then
+        inst.fires = {}
+
+        for i, fx_prefab in ipairs(inst:GetSkinName() == nil and { "lighterfire" } or SKIN_FX_PREFAB[inst:GetSkinName()] or {}) do
+            local fx = SpawnPrefab(fx_prefab)
+            fx.entity:SetParent(owner.entity)
+            fx.entity:AddFollower()
+            fx.Follower:FollowSymbol(owner.GUID, "swap_object", fx.fx_offset_x, fx.fx_offset_y, 0)
+            fx:AttachLightTo(owner)
+
+            table.insert(inst.fires, fx)
+        end
+    end
+
+	if owner:HasTag("pyromaniac") then
+		if inst.components.spellcaster == nil then
+			inst:AddComponent("spellcaster")
+			inst.components.spellcaster:SetSpellFn(createlight)
+			inst.components.spellcaster.canusefrominventory = true
+		end
+		inst.components.fueled:StopConsuming()
+	else
+		inst:RemoveComponent("spellcaster")
+	end
+
+
+    --[[if inst.fire == nil then
+        inst.fire = SpawnPrefab("lighterfire")
+        --inst.fire.Transform:SetScale(.125, .125, .125)
+        inst.fire.entity:AddFollower()
+        inst.fire.Follower:FollowSymbol(owner.GUID, "swap_object", 56, -40, 0)
+    end]]
 end
 
 env.AddPrefabPostInit("lighter", function(inst)
