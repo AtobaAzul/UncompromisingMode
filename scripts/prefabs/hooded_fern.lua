8407afe9d1afaed66e60a6189bc2244ecc7144a7
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2020-10-09 18:39:13 -0500

    Hooded Ferns Give 2 Foliage

--- scripts/prefabs/hooded_fern.lua
+++ scripts/prefabs/hooded_fern.lua
@@ -96,7 +96,7 @@ local function grass(name, stage)
         inst:AddComponent("pickable")
         inst.components.pickable.picksound = "dontstarve/wilson/pickup_reeds"
 
-        inst.components.pickable:SetUp("foliage", TUNING.GRASS_REGROW_TIME)
+        inst.components.pickable:SetUp("foliage", TUNING.GRASS_REGROW_TIME,2)
         inst.components.pickable.onregenfn = onregenfn
         inst.components.pickable.onpickedfn = onpickedfn
         inst.components.pickable.makeemptyfn = makeemptyfn
