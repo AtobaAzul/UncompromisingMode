8ef9071915d6c3484315e651f45b6aef816d1270
Scrimbles <40772985+Shodance@users.noreply.github.com>
2020-10-09 18:35:00 -0400

    cave stuff

--- postinit/prefabs/cave_network.lua
+++ postinit/prefabs/cave_network.lua
@@ -3,7 +3,9 @@ GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------
 local function OnSeasonTick(inst)
 	if TheWorld.state.issummer then
-		inst.components.worldtemperature:SetTemperatureMod(TUNING.DSTU.CAVES_TEMP_MULT, TUNING.DSTU.CAVES_TEMP_LOCUS)
+		inst.components.worldtemperature:SetTemperatureMod(TUNING.DSTU.SUMMER_CAVES_TEMP_MULT, TUNING.CAVES_TEMP_LOCUS)
+	elseif TheWorld.state.iswinter then
+		inst.components.worldtemperature:SetTemperatureMod(TUNING.DSTU.WINTER_CAVES_TEMP_MULT, TUNING.CAVES_TEMP_LOCUS)
 	else
 		inst.components.worldtemperature:SetTemperatureMod(TUNING.CAVES_TEMP_MULT, TUNING.CAVES_TEMP_LOCUS)
 	end
@@ -13,6 +15,8 @@ env.AddPrefabPostInit("cave_network", function(inst)
 	if not TheWorld.ismastersim then
 		return
 	end
+
 	inst:DoTaskInTime(0, OnSeasonTick)
+	inst:ListenForEvent("ms_playerjoined", OnSeasonTick)
 	inst:ListenForEvent("seasontick", OnSeasonTick)
 end)
