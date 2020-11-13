ae0269db3a79780dc5dc8e8a78c968cd98c638d4
Scrimbles <40772985+Shodance@users.noreply.github.com>
2020-11-13 00:04:15 -0500

    wings stat changes

--- scripts/prefabs/beefalowings.lua
+++ scripts/prefabs/beefalowings.lua
@@ -42,8 +42,8 @@ local function fn()
 	inst.components.inventoryitem.atlasname = "images/inventoryimages/beefalowings.xml"
     inst:AddComponent("edible")
     inst.components.edible.healthvalue = 30
-    inst.components.edible.hungervalue = 50
-    inst.components.edible.sanityvalue = 5
+    inst.components.edible.hungervalue = TUNING.CALORIES_SMALL*5
+    inst.components.edible.sanityvalue = 30
     inst.components.edible.foodtype = FOODTYPE.MEAT
 
     inst:AddComponent("perishable")
