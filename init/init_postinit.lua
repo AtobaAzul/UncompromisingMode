358469349abb77a8cd20286352125b18dacd1abf
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2021-01-21 09:34:07 -0600

    Uncommented ruinsstatues postinit

--- init/init_postinit.lua
+++ init/init_postinit.lua
@@ -116,7 +116,7 @@ local prefab_post = {
 	"tophat",
 	"tophatreduction",
 	"crabking",
-	--"ruinsstatues", Commented out until time for testing AT
+	"ruinsstatues", 
 	"chessjunk",
 	"walrus_camp",
 }
