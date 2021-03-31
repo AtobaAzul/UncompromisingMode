ca57bdc1d5be58e54014949d29884d1571b26b3f
Scrimbles <40772985+Shodance@users.noreply.github.com>
2021-03-30 20:32:41 -0400

    Gore horn now drains when running at collision speed, has a 3 second delay in refueling after draining fuel, movement speed is now reset when colliding with any object, no longer collide with diggable objects

--- scripts/prefabs/gore_horn_hat.lua
+++ scripts/prefabs/gore_horn_hat.lua
@@ -16,6 +16,39 @@ local EFFECTS =
     cold = "dr_warm_loop_1",
 }
 
+local function fuelme(inst)
+	if inst.components.fueled:GetPercent() < 1 then
+		if inst.pausedfuel then
+			inst.components.fueled:DoDelta(10)
+		end
+		if inst.components.fueled:GetPercent() >= 1 then
+			if inst.fuelmetask ~= nil then
+				inst.fuelmetask:Cancel()
+				inst.fuelmetask = nil
+			end
+		end
+	else
+		if inst.fuelmetask ~= nil then
+			inst.fuelmetask:Cancel()
+			inst.fuelmetask = nil
+		end
+	end
+end
+
+local function reducespeed(inst)
+	inst.runspeed = 1
+	if inst.physbox ~= nil then
+		inst.physbox = nil
+	end
+	inst.SoundEmitter:KillSound("gorehorn")
+end
+
+local function unpausefueled(inst)
+	inst.pausedfuel = true
+	if inst.fuelmetask == nil then
+		inst.fuelmetask = inst:DoPeriodicTask(0.5, fuelme)
+	end
+end
 
 local function speedcheck(inst)
 
@@ -44,7 +77,7 @@ local function speedcheck(inst)
 			inst.angleadjustment2 = -360
 		end
 		
-		if inst.components.inventory and inst.components.inventory:EquipHasTag("gore_horn") and inst.sg:HasStateTag("moving") and inst.facing_angle ~= nil and inst.facing_angle_old ~= nil and
+		if inst.gorehorn ~= nil and inst.gorehorn.components.fueled:GetPercent() > 0 and inst.components.inventory and inst.components.inventory:EquipHasTag("gore_horn") and inst.sg:HasStateTag("moving") and inst.facing_angle ~= nil and inst.facing_angle_old ~= nil and
 		(inst.facing_angle >= inst.facing_angle_old + inst.angleadjustment2 - 10 and inst.facing_angle <= inst.facing_angle_old + inst.angleadjustment1 + 10)
 		and inst.components.locomotor ~= nil
 		and not inst.components.rider:IsRiding() then
@@ -58,41 +91,41 @@ local function speedcheck(inst)
 				end
 			end
 			
-			if inst.runspeed >= 2.5 and inst.physbox == nil then
+			if inst.runspeed >= 2 and inst.physbox == nil then
 				inst.physbox = SpawnPrefab("gore_horn_physbox")
-				--inst.physbox.AnimState:SetFinalOffset(-1)
-				--inst.physbox.Physics:Teleport(0,0,0)
 				inst.physbox.owner = inst
 				inst.physbox.entity:AddFollower()
-				inst.physbox.Follower:FollowSymbol(inst.GUID, "swap_hat", 0, 80, 0)
+				inst.physbox.Follower:FollowSymbol(inst.GUID, "swap_hat", 0, 90, 0)
+				
+				inst:ListenForEvent("onremove", function(inst) 
+					if inst.runspeed ~= nil then 
+						inst.runspeed = 1
+					end
+				end, inst.physbox)
+				inst:ListenForEvent("gore_horn_collision", reducespeed)
 			
-				--inst:AddChild(inst.physbox)
 				inst.SoundEmitter:PlaySound("dontstarve/creatures/rook_nightmare/charge_LP", "gorehorn")	
 			end
 			
-			--[[if inst.runspeed < 0.25 and inst.task ~= nil then
-				inst.task:Cancel()
-				inst.task = nil
-			end]]
-			
-			--inst.SoundEmitter:SetParameter("gorehorn", "intensity", inst.runspeed / 2.5)
 			if inst.task == nil then
 				inst.task = inst:DoPeriodicTask(0.27, function(inst) 
-					inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/steam", nil, inst.runspeed / 2.5) 
-					if inst.runspeed > 2.3 then
+					inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/steam", nil, inst.runspeed / 2) 
+					if inst.runspeed > 1.7 then
 						SpawnPrefab("ground_chunks_breaking").Transform:SetPosition(inst.Transform:GetWorldPosition())
 					end
-					
-					--[[if not inst.sg:HasStateTag("moving") then
-						inst.task:Cancel()
-						inst.task = nil
-					end]]
 				end)
 			end
-			--inst.SoundEmitter:KillSound("gorecharge")
-            --inst.SoundEmitter:PlaySound("dontstarve/creatures/rook_nightmare/charge_LP", "gorecharge", inst.runspeed / 2.5)
-			--inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/steam", nil, inst.runspeed / 2.5)
+			
 			inst.components.locomotor:SetExternalSpeedMultiplier(inst, "gore_horn", inst.runspeed)
+			if inst.runspeed >= 2 then
+				inst.gorehorn.components.fueled:DoDelta(-1)
+				inst.gorehorn.pausedfuel = false
+				if inst.gorehorn.unpausefueledtask ~= nil then
+					inst.gorehorn.unpausefueledtask:Cancel()
+					inst.gorehorn.unpausefueledtask = nil
+				end
+				inst.gorehorn.unpausefueledtask = inst.gorehorn:DoTaskInTime(3, unpausefueled)
+			end
 		else
 			if inst.physbox ~= nil then
 				inst.physbox.AnimState:PlayAnimation("close")
@@ -100,7 +133,7 @@ local function speedcheck(inst)
 				inst.physbox = nil
 				inst.SoundEmitter:KillSound("gorehorn")
 			end
-            --inst.SoundEmitter:KillSound("gorecharge")
+
 			if inst.task ~= nil then
 				inst.task:Cancel()
 				inst.task = nil
@@ -109,6 +142,8 @@ local function speedcheck(inst)
 			
 			inst.runspeed = 1
 			inst.components.locomotor:RemoveExternalSpeedMultiplier(inst, "gore_horn") 
+			
+			inst:RemoveEventCallback("gore_horn_collision", reducespeed)
 		end
 	else
 		inst.binarytoggle = true
@@ -129,7 +164,13 @@ end
 			owner.AnimState:Show("HEAD_HAT")
 		end
 		
+		owner.gorehorn = inst
+		
 		owner:ListenForEvent("locomote", speedcheck)
+	
+		if inst.fuelmetask == nil then
+			inst.fuelmetask = inst:DoPeriodicTask(0.5, fuelme)
+		end
     end
 
 	local function onunequip(inst, owner)
@@ -148,6 +189,21 @@ end
 		owner:RemoveEventCallback("locomote", speedcheck)
 		
 		owner.components.locomotor:RemoveExternalSpeedMultiplier(owner, "gore_horn") 
+		
+		if inst.fuelmetask == nil then
+			inst.fuelmetask = inst:DoPeriodicTask(0.5, fuelme)
+		end
+	
+    end
+	
+	local function stoprunning(inst)
+		inst.pausedfuel = true
+    end
+	
+	local function checkiffull(inst)
+		if inst.components.fueled:GetPercent() >= 1 then
+			inst.pausedfuel = false
+		end
     end
 
 	local function fn()
@@ -182,6 +238,21 @@ end
 		inst.components.inventoryitem.atlasname = "images/inventoryimages/snowgoggles.xml"
 
         inst:AddComponent("inspectable")
+		
+		inst:AddComponent("fueled")
+		inst.components.fueled:InitializeFuelLevel(100)
+		inst.components.fueled.accepting = false
+		inst.components.fueled:SetDepletedFn(stoprunning)
+		inst:ListenForEvent("percentusedchange", checkiffull)
+		
+		inst.fueltask = nil
+		inst.fuelmetask = nil
+		inst.pausedfuel = true
+		inst.readytorun = true
+		
+		if inst.fuelmetask == nil then
+			inst.fuelmetask = inst:DoPeriodicTask(0.5, fuelme)
+		end
 
         inst:AddComponent("equippable")
         inst.components.equippable.equipslot = EQUIPSLOTS.HEAD
@@ -202,40 +273,52 @@ end
 		if not other:IsValid() or inst.recentlycharged[other] then
 			return
 		elseif other:HasTag("smashable") and other.components.health ~= nil then
-			--other.Physics:SetCollides(false)
 			other.components.health:Kill()
+			
 			ShakeAllCameras(CAMERASHAKE.SIDE, .5, .05, .1, inst, 40)
+			
+			inst.AnimState:PlayAnimation("close")
+			inst:DoTaskInTime(0.3, inst.Remove)
+			
+			if inst.owner ~= nil then
+				inst.owner:PushEvent("gore_horn_collision")
+			end
 		elseif other.components.workable ~= nil
 			and other.components.workable:CanBeWorked()
 			and other.components.workable.action ~= ACTIONS.NET then
 			SpawnPrefab("collapse_small").Transform:SetPosition(other.Transform:GetWorldPosition())
 			other.components.workable:Destroy(inst)
-			ShakeAllCameras(CAMERASHAKE.SIDE, .5, .05, .1, inst, 40)
 			if other:IsValid() and other.components.workable ~= nil and other.components.workable:CanBeWorked() then
 				inst.recentlycharged[other] = true
 				inst:DoTaskInTime(3, ClearRecentlyCharged, other)
+			end
+			
+			ShakeAllCameras(CAMERASHAKE.SIDE, .5, .05, .1, inst, 40)
+			
+			inst.AnimState:PlayAnimation("close")
+			inst:DoTaskInTime(0.3, inst.Remove)
+			
+			if inst.owner ~= nil then
+				inst.owner:PushEvent("gore_horn_collision")
 			end
 		elseif other.components.health ~= nil and not other.components.health:IsDead() then
 			inst.recentlycharged[other] = true
 			inst:DoTaskInTime(3, ClearRecentlyCharged, other)
 			inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/explo")
-            other.components.combat:GetAttacked(inst, 50, nil)
+            other.components.combat:GetAttacked(inst, 100, nil)
+			
 			ShakeAllCameras(CAMERASHAKE.SIDE, .5, .05, .1, inst, 40)
+			
+			inst.AnimState:PlayAnimation("close")
+			inst:DoTaskInTime(0.3, inst.Remove)
+			
+			if inst.owner ~= nil then
+				inst.owner:PushEvent("gore_horn_collision")
+			end
 		end
 	end
 	
-	--[[local function oncollide(inst, other)
-		if not (other ~= nil and other:IsValid() and inst:IsValid())
-			or inst.recentlycharged[other]
-			or other:HasTag("player") then
-			--or Vector3(inst.Physics:GetVelocity()):LengthSq() < 42 then
-			return
-		end
-		ShakeAllCameras(CAMERASHAKE.SIDE, .5, .05, .1, inst, 40)
-		inst:DoTaskInTime(2 * FRAMES, onothercollide, other)
-	end]]
-	
-	local NOTAGS = { "fx", "INLIMBO", "shadow", "player" }
+	local NOTAGS = { "fx", "INLIMBO", "shadow", "player", "DIG_workable" }
 	local function oncollide(inst)
 		if inst.owner ~= nil then
 			local x, y, z = inst.owner.Transform:GetWorldPosition()
@@ -258,17 +341,8 @@ end
 		inst.AnimState:SetBuild("forcefield")
 		inst.AnimState:PlayAnimation("open")
 		inst.AnimState:PushAnimation("idle_loop", true)
-		
-		--MakeCharacterPhysics(inst, 50, 1.5)
 
-		--[[inst.Physics:SetCollisionGroup(COLLISION.ITEMS)
-		inst.Physics:ClearCollisionMask()
-		inst:SetPhysicsRadiusOverride(10)
-		inst.Physics:CollidesWith(COLLISION.OBSTACLES)
-		inst.Physics:CollidesWith(COLLISION.CHARACTERS)
-		inst.Physics:SetCapsule(1.5, 1.5)]]
-
-		--inst:AddTag("fx")
+		inst:AddTag("fx")
 		
 		inst:AddTag("NOCLICK")
 
@@ -277,18 +351,12 @@ end
         if not TheWorld.ismastersim then
             return inst
         end
-
-		--[[inst:AddComponent("locomotor")
-		inst.recentlycharged = {}
-		inst.Physics:SetCollisionCallback(oncollide)
-		inst:ListenForEvent("on_collide", oncollide)]]
 		
 		inst.owner = nil
 		
 		inst.recentlycharged = {}
 		inst:DoPeriodicTask(0.2, oncollide)
-		
-		
+	
 		inst.persists = false
 
         return inst
