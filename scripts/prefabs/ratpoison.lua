c80b4c958cb73e8a7c588a14030fd40d507e48ab
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2021-04-22 17:52:14 -0500

    Made rat poison removable, made it spawn on the ground, random rotations and somewhat random scale, made it darker.

--- scripts/prefabs/ratpoison.lua
+++ scripts/prefabs/ratpoison.lua
@@ -19,6 +19,34 @@ local function oneaten(inst, eater)
 	end
 end
 
+local function OnSave(inst,data)
+if inst.rotation ~= nil then
+data.rotation = inst.rotation
+end
+if inst.scalex ~= nil then
+data.scalex = inst.scalex
+end
+if inst.scalez ~= nil then
+data.scalez = inst.scalez
+end
+end
+
+local function OnLoad(inst,data)
+if data.rotation ~= nil then
+inst.rotation = data.rotation
+inst.Transform:SetRotation(inst.rotation)
+end
+if data.scalex ~= nil and data.scalez ~= nil then
+inst.scalex = data.scalex
+inst.scalez = data.scalez
+inst.Transform:SetScale(inst.scalex,1,inst.scalez)
+end
+end
+
+local function OnPicked(inst)
+inst:Remove()
+end
+
 local function fn()
     local inst = CreateEntity()
     inst.entity:AddTransform()
@@ -30,7 +58,7 @@ local function fn()
 
     inst.AnimState:SetLayer(LAYER_BACKGROUND)
     inst.AnimState:SetSortOrder(3)
-
+	inst.AnimState:SetOrientation(ANIM_ORIENTATION.OnGround)
     inst.AnimState:SetBank("tar_trap")
     inst.AnimState:SetBuild("tar_trap")
 
@@ -51,18 +79,36 @@ local function fn()
 	
 	inst:AddComponent("perishable")
 	inst.components.perishable:SetPerishTime(TUNING.PERISH_SUPERSLOW)
-	
 	inst:AddComponent("bait")
+	
 
+	
+	inst:AddComponent("pickable")
+
+    inst.components.pickable.onpickedfn = OnPicked
+    inst.components.pickable:SetUp(nil,0)
+    inst.components.pickable.transplanted = true
+	
+	inst.AnimState:SetMultColour(0.6,1,1,1)
+	inst.OnSave = OnSave
+	inst.OnLoad = OnLoad
     return inst
 end
 
 local function OnDeploy(inst, pt)
-	SpawnPrefab("ratpoison").Transform:SetPosition(pt.x, 0, pt.z)
-	SpawnPrefab("ratpoison").Transform:SetPosition(pt.x, 0, pt.z)
-	SpawnPrefab("ratpoison").Transform:SetPosition(pt.x, 0, pt.z)
-	SpawnPrefab("ratpoison").Transform:SetPosition(pt.x, 0, pt.z)
+	local rotation = math.random(-180,180)
+	local scalex = math.random(0.7,0.9)
+	local scalez = math.random(0.8,1.1)
+	for i = 1,4 do 
+	local poison = SpawnPrefab("ratpoison")
+	poison.rotation = rotation
+	poison.scalex = scalex
+	poison.scalez = scalez
+	poison.Transform:SetPosition(pt.x, 0, pt.z)
+	poison.Transform:SetRotation(poison.rotation)
+	poison.Transform:SetScale(poison.scalex,1,poison.scalez)
     inst:Remove()
+	end
 end
 
 local function itemfn()
