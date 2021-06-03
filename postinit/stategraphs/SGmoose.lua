ae237f08116244a6ced12d5e5f93ff89b22ed559
Scrimbles <40772985+Shodance@users.noreply.github.com>
2021-06-03 00:04:53 -0400

    Fixed gmeese ground pounding allies when the no damage ground pound change was made

--- postinit/stategraphs/SGmoose.lua
+++ postinit/stategraphs/SGmoose.lua
@@ -308,7 +308,7 @@ local states = {
 		{
 			TimeEvent(2*FRAMES, function(inst) 
 				inst.components.groundpounder:GroundPound()
-				inst.components.combat:DoAreaAttack(inst, TUNING.MOOSE_ATTACK_RANGE * 1.1) --GroundPound Is purely visual
+				inst.components.combat:DoAreaAttack(inst, TUNING.MOOSE_ATTACK_RANGE * 1.1, nil, nil, nil, { "moose", "mossling" }) --GroundPound Is purely visual
 				inst.SoundEmitter:PlaySound("dontstarve_DLC001/creatures/dragonfly/land") 
 			end)
 		},
