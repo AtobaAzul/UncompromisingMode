f404f0a07774d7e4de70fe8118d8870f0b8d6b7e
Scrimbles <40772985+Shodance@users.noreply.github.com>
2021-08-15 16:08:10 -0400

    sdaghrfageragfdgfdsgdfsfdgsgfds

--- init/init_food/init_crockpot.lua
+++ init/init_food/init_crockpot.lua
@@ -93,7 +93,7 @@ AddIngredientValues({"forgetmelots"}, {decoration=1, foliage=1})
 -- Recipe changes (mostly added above filler changes)
 -- But made to be easily customised below for future changes
 -----------------------------------------------------------------
-recipes.monsterlasagna.test = function(cooker, names, tags) return tags.monster and (TUNING.DSTU.CROCKPOTMONSTMEAT and tags.meat and (tags.monster >= 3 or tags.monster >= tags.meat) or not TUNING.DSTU.CROCKPOTMONSTMEAT and tags.monster >= 2) and not tags.inedible and not (tags.insectoid and tags.insectoid >= 1) and LimitIceTestFn(tags, RECIPE_ICE_LIMIT) end 
+recipes.monsterlasagna.test = function(cooker, names, tags) return tags.monster and (TUNING.DSTU.CROCKPOTMONSTMEAT and tags.meat and (tags.monster >= 3 or tags.monster > tags.meat) or not TUNING.DSTU.CROCKPOTMONSTMEAT and tags.monster >= 2) and not tags.inedible and not (tags.insectoid and tags.insectoid >= 1) and LimitIceTestFn(tags, RECIPE_ICE_LIMIT) end 
 -- Original:           test = function(cooker, names, tags) return tags.monster and tags.monster >= 2 and not tags.inedible end,    
 
 recipes.butterflymuffin.test = function(cooker, names, tags) return (names.butterflywings or names.moonbutterflywings) and not tags.meat and tags.veggie and UncompromisingFillers(tags) end 
