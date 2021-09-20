f3dae16244ea0aa7e275ef8e7e49b97971e1d146
Scrimbles <40772985+Shodance@users.noreply.github.com>
2021-09-20 18:54:37 -0400

    aigh koreans changes are active when food regen is turned offfffffffffffffffffff

--- postinit/components/eater.lua
+++ postinit/components/eater.lua
@@ -8,8 +8,10 @@ AddComponentPostInit("eater", function(self)
             health_delta, hunger_delta, sanity_delta = _custom_stats_mod_fn(inst, health_delta, hunger_delta, sanity_delta, food, feeder)
         end
 
-        sanity_delta = food.components.edible:GetSanity(inst)
-
+		if not inst:HasTag("UM_foodregen") and not inst:HasTag("vetcurse") then
+			sanity_delta = food.components.edible:GetSanity(inst)
+		end
+		
         return health_delta, hunger_delta, sanity_delta
     end
 end)
