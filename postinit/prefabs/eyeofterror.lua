6e4f223c5709105a057cbba25ef717fd620537b0
Scrimbles <40772985+Shodance@users.noreply.github.com>
2021-12-04 13:38:27 -0500

    Twins now scale their spawn rate of mini eyes based on nearby players. 40 is for solo players, and that is decreased by 10 seconds per additional player, down to 18 seconds (the default spawn rate)

--- postinit/prefabs/eyeofterror.lua
+++ postinit/prefabs/eyeofterror.lua
@@ -1,6 +1,21 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
+local function UpdateCooldown(inst)
+	local x, y, z = inst.Transform:GetWorldPosition()
+	
+	local playercount = #TheSim:FindEntities(x, y, z, 40, {"player"}, {"playerghost"}) or 1
+	local playerscaling = 10 * (playercount or 1)
+
+	inst._cooldowns.spawn = 50 - playerscaling
+	
+	if inst._cooldowns.spawn <= 20 then
+		inst._cooldowns.spawn = 18
+	end
+	
+	print(inst._cooldowns.spawn)
+end
+
 local function OnAttackedTwin1(inst, data)
     if data.attacker and not inst.components.commander:IsSoldier(data.attacker) then
 		inst.components.combat:ShareTarget(data.attacker, 50, function(dude)
@@ -19,6 +34,8 @@ env.AddPrefabPostInit("twinofterror1", function(inst)
 	end
 	
     inst:ListenForEvent("attacked", OnAttackedTwin1)
+	
+	inst:DoPeriodicTask(5, UpdateCooldown)
 end)
 
 local function OnAttackedTwin2(inst, data)
@@ -38,4 +55,6 @@ env.AddPrefabPostInit("twinofterror2", function(inst)
 	end
 	
     inst:ListenForEvent("attacked", OnAttackedTwin2)
+	
+	inst:DoPeriodicTask(5, UpdateCooldown)
 end)
