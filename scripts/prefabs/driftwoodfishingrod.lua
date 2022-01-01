b17d8e9a5c80df2263fca770808f0de919cfeda3
Wackybloodwing <82704427+Wackybloodwing@users.noreply.github.com>
2021-12-31 22:54:39 -0700

    fixed?

--- scripts/prefabs/driftwoodfishingrod.lua
+++ scripts/prefabs/driftwoodfishingrod.lua
@@ -73,7 +73,7 @@ local function fn()
 
     inst:AddComponent("inspectable")
     inst:AddComponent("inventoryitem")
-	inst.components.inventoryitem.atlasname = "images/inventoryimages/driftwoodfishingrod.tex"
+	inst.components.inventoryitem.atlasname = "images/inventoryimages/driftwoodfishingrod.xml"
 
     inst:AddComponent("equippable")
     inst.components.equippable:SetOnEquip(onequip)
