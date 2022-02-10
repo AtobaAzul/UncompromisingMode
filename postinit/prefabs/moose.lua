e4583ce4e3f70189351893a26fe67a5aad902ea0
TheAxeSwingingSmasher <56132394+TheAxeSwingingSmasher@users.noreply.github.com>
2022-02-10 13:03:15 -0600

    follow up to above

--- postinit/prefabs/moose.lua
+++ postinit/prefabs/moose.lua
@@ -66,10 +66,6 @@ env.AddPrefabPostInit("moose", function(inst)
         inst.components.combat:SetAreaDamage(TUNING.DEERCLOPS_AOE_RANGE/2, TUNING.DEERCLOPS_AOE_SCALE, isnotmossling) -- you can edit these values to your liking -Axe
 	end       
 
-	if inst.components.combat ~= nil then
-		inst.components.combat:SetRange(TUNING.MOOSE_ATTACK_RANGE * 1.1)
-	end
-	
     inst:AddComponent("groundpounder")
     inst.components.groundpounder.destroyer = false
     inst.components.groundpounder.damageRings = 0
