878059b8640646c7700df11e06258c3ea0b736b9
AtobaAzul <atoba.azul.radical@gmail.com>
2022-02-27 09:55:45 -0300

    Poison toads config

--- postinit/prefabs/frog.lua
+++ postinit/prefabs/frog.lua
@@ -11,7 +11,7 @@ SetSharedLootTable('frog',
 })
 ]]
 local function OnIsAutumn(inst, isautumn)
-    if isautumn and TheWorld.state.cycles >= TUNING.DSTU.WEATHERHAZARD_START_DATE_AUTUMN then
+    if isautumn and TheWorld.state.cycles >= TUNING.DSTU.WEATHERHAZARD_START_DATE_AUTUMN and TUNING.DSTU.TOADS then
 		inst.components.lootdropper:SetChanceLootTable('uncompromising_toad')
         inst.AnimState:SetBuild("frog_yellow_build")
 		inst:SetPrefabNameOverride("uncompromising_toad")
