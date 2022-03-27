96e48a645daa3fec38327dc213a7522152400d92
AtobaAzul <atoba.azul.radical@gmail.com>
2022-03-27 14:58:24 -0300

    Increase Version

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "󰀕 Uncompromising Mode"
 description = 
 [[
-󰀔 [ Version 1.4.4.41 : "Squeaky Clean" ]
+󰀔 [ Version 1.4.4.5 : "Squeaky Clean" ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -17,7 +17,7 @@ Prominent features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Squeaky Clean v1.4.4.41"
+version = "Squeaky Clean v1.4.4.5"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
