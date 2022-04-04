b62aae2ee66ecca6f4014cf07af9acca2b306e9b
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-04-03 21:45:26 -0400

    1.4.4.61

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "󰀕 Uncompromising Mode"
 description = 
 [[
-󰀔 [ Version 1.4.4.6 : "Squeaky Clean" ]
+󰀔 [ Version 1.4.4.61 : "Squeaky Clean" ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -17,7 +17,7 @@ Prominent features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Squeaky Clean v1.4.4.6"
+version = "Squeaky Clean v1.4.4.61"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
