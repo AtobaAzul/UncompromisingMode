123bf4d3117cc7cd3b2d75bd947b674072dcbf69
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-04-23 12:19:13 -0400

    gmoose prioritizes: 1) chasing and fighting 2) laying an egg 3) facing nearby players 4) leaving if it can leave 5) going back to its spawn point / egg
    (cherry picked from commit 92e200882cedbb737cebc7f620eded3a715ed4cf)

--- postinit/brains/moosebrain.lua
+++ postinit/brains/moosebrain.lua
@@ -2,11 +2,39 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 require "behaviours/chaseandattack"
 
+local START_FACE_DIST = 15
+local KEEP_FACE_DIST = 20
+
+local function LayEgg(inst)
+    return inst.WantsToLayEgg
+        and not inst.components.entitytracker:GetEntity("egg")
+        and BufferedAction(inst, nil, ACTIONS.LAYEGG)
+        or nil
+end
+
+local function GetFaceTargetFn(inst)
+    if inst.sg:HasStateTag("busy") then
+        return
+    end
+    local target = FindClosestPlayerToInst(inst, START_FACE_DIST, true)
+    return target ~= nil and not target:HasTag("notarget") and target or nil
+end
+
+local function KeepFaceTargetFn(inst, target)
+    return not (inst.sg:HasStateTag("busy") or
+                inst:HasTag("notarget"))
+        and inst:IsNear(target, KEEP_FACE_DIST)
+end
+
 local function WhyAreYouRunning(self)
 					
 	local FightMe = ChaseAndAttack(self.inst)
+	local LayEgg = DoAction(self.inst, LayEgg)
+	local FaceMe = FaceEntity(self.inst, GetFaceTargetFn, KeepFaceTargetFn)
 					
     table.insert(self.bt.root.children, 1, FightMe)
+    table.insert(self.bt.root.children, 2, LayEgg)
+    table.insert(self.bt.root.children, 3, FaceMe)
 end
 
 
