68938ab76a6e07b76dab3f4649d76274e1f40a44
AtobaAzul <atoba.azul.radical@gmail.com>
2022-06-22 16:47:28 -0300

    spirit's wanda quotes

--- init/init_descriptions/wanda.lua
+++ init/init_descriptions/wanda.lua
@@ -10,9 +10,9 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
     ANNOUNCE.ANNOUNCE_RATRAID_SPAWN = "Why now?!"
     ANNOUNCE.ANNOUNCE_RATRAID_OVER = "Those were important materials! I think..."
     ANNOUNCE.ANNOUNCE_ACIDRAIN = {
-        "Timefall!",
+        "This rain is not normal, watch out!",
         "Ow! That stings!",
-        "This rain is doing bad things to my timeline...",
+        "This rain nothing but trouble!",
     }
     ANNOUNCE.ANNOUNCE_TOADSTOOLED = "Something ominous stirs."
 	--FoodBuffs
@@ -27,41 +27,41 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	--FoodBuffs
 
 	--CaliforniaKing
-	ANNOUNCE.ANNOUNCE_ATTACH_BUFF_CALIFORNIAKING = "I feel sick..."
-	ANNOUNCE.ANNOUNCE_DETACH_BUFF_CALIFORNIAKING = "Ah, time heals all... well, MOST wounds."
+	ANNOUNCE.ANNOUNCE_ATTACH_BUFF_CALIFORNIAKING = "Oh botheration..."
+	ANNOUNCE.ANNOUNCE_DETACH_BUFF_CALIFORNIAKING = "I feel like rewinding to a point before I ate that."
 	DESCRIBE.CALIFORNIAKING = "There's no timeline where I'd find this appealing."
 	--CaliforniaKing
-	
+
     DESCRIBE.UNCOMPROMISING_RAT = "Get away from my clocks!"
-    DESCRIBE.UNCOMPROMISING_RATHERD = "I think they just wanted an escape from the Ratacombs."
-    DESCRIBE.UNCOMPROMISING_RATBURROW = "I think they just wanted an escape from the Ratacombs."
+    DESCRIBE.UNCOMPROMISING_RATHERD = "I don't even remember there being so many."
+    DESCRIBE.UNCOMPROMISING_RATBURROW = "Is this where you kept my clocks?"
     DESCRIBE.UNCOMPROMISING_WINKYBURROW = "It wasn't made to last."
     DESCRIBE.UNCOMPROMISING_WINKYHOMEBURROW = "At least this one is helping. Although I'd rather not risk it."
-	
+
 	DESCRIBE.WINKY =
         {
             GENERIC = "I think something went wrong in the timeline.",
-            ATTACKER = "Careful, %s!",
+            ATTACKER = "What do you think you're doing, %s?",
             MURDERER = "This is the worst possible timeline!",
             REVIVER = "Thank you, %s, but I've still got my eye on you.",
             GHOST = "Maybe it's better this way.",
             FIRESTARTER = "She's left destruction in her wake!",
         }
-		
+
     DESCRIBE.RATPOISONBOTTLE = "Sorry it had to come to this."
     DESCRIBE.RATPOISON = "Your time has come, little ones..."
 
     DESCRIBE.MONSTERSMALLMEAT = "A little poison is still poison."
-    DESCRIBE.COOKEDMONSTERSMALLMEAT = "Yes, it's STILL poisonous."
-    DESCRIBE.MONSTERSMALLMEAT_DRIED = "Maybe the poison all blew away in the wind?"
+    DESCRIBE.COOKEDMONSTERSMALLMEAT = "Either I rushed cooking it or it is still just that bad."
+    DESCRIBE.MONSTERSMALLMEAT_DRIED = "Hopefully it has gotten better after all this time."
 
     DESCRIBE.UM_MONSTEREGG = "What might it hatch into?"
     DESCRIBE.UM_MONSTEREGG_COOKED = "Undercooking or overcooking wouldn't have mattered."
 
     DESCRIBE.MUSHROOMSPROUT_OVERWORLD = "The worst of the worst."
     DESCRIBE.TOADLING = "Offspring? That's...new."
-	
-    DESCRIBE.GASMASK = "Oh botheration, I'll look like a fool wearing that!"
+
+    DESCRIBE.GASMASK = "I never really wore masks before, I wonder how I would look..."
 	DESCRIBE.MOCK_DRAGONFLY = DESCRIBE.DRAGONFLY
 	DESCRIBE.MOTHERGOOSE = DESCRIBE.MOOSE
 	DESCRIBE.SPIDERQUEENCORPSE = "It's only a matter of time before something worse happens..."
@@ -83,23 +83,23 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
     }
 	ANNOUNCE.ANNOUNCE_OVER_EAT =
 	{
-		STUFFED = "wacky's making inflation jokes again",
-		OVERSTUFFED = "OAGH STAHP",
+		STUFFED = "wacky's inflation fantasy has taken over",
+		OVERSTUFFED = "BRAPPING",
     }
 	--DESCRIBE.SAND = "Lots of tiny, tiny stones."
 	--DESCRIBE.SANDHILL = "The sand has formed a small pile."
 	DESCRIBE.SNOWPILE = "Time to clean shop!"
 	DESCRIBE.SNOWGOGGLES = "My future is a little more clear."
-	
-	DESCRIBE.SNOWMONG = "Enchanted snow? What a waste of magic..."
-	DESCRIBE.SHOCKWORM = "I told you, electricity will be the end of us!"
-	DESCRIBE.ZASPBERRY = "Electricity... yeah, big deal."
-	DESCRIBE.ZASPBERRYPARFAIT = "A mouth tingling desert!"
-	DESCRIBE.ICEBOOMERANG = "I wonder if cryostasis would buy me more time..."
+
+	DESCRIBE.SNOWMONG = "Maybe those piles were worth my time after all!"
+	DESCRIBE.SHOCKWORM = "You were not the right berry!"
+	DESCRIBE.ZASPBERRY = "Could there be a clockwork berry?"
+	DESCRIBE.ZASPBERRYPARFAIT = "A mouth tingling dessert!"
+	DESCRIBE.ICEBOOMERANG = "It could freeze me in time if I don't use it right."
 	DESCRIBE.MINOTAUR_BOULDER = "Stop! You might bury this whole place!"
-	DESCRIBE.MINOTAUR_BOULDER_BIG = "This might help me clean his clocks!"
+	DESCRIBE.MINOTAUR_BOULDER_BIG = "Oooh, you are making a huge mess!"
 	DESCRIBE.SNOWBALL_THROWABLE = "I don't have TIME to play!"
-	DESCRIBE.VETERANSHRINE = "It's always tempting, but do I have more time to waste?"
+	DESCRIBE.VETERANSHRINE = "I don't remember anything like you..."
 	DESCRIBE.RAT_TAIL = "It just... popped right off!"
 	DESCRIBE.PLAGUEMASK = "Not much of a birds eye view."
 	DESCRIBE.SPIDER_TRAPDOOR = "So, it just waits around all day for someone to pass by?"
@@ -107,20 +107,25 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.HOODEDTRAPDOOR = "It hasn't been here long, but moss is already growing on it"
 	DESCRIBE.WICKER_TENTACLE = "It's been severed from the collective, and it's times almost up."
 	DESCRIBE.HONEY_LOG = "That's... sweet."
-	
+
 	DESCRIBE.BUSHCRAB = "AAH! When did you get here?!"
 	DESCRIBE.LAVAE2 = DESCRIBE.LAVAE
 	DESCRIBE.DISEASECUREBOMB = "Reverses the disease."
 	DESCRIBE.SHROOM_SKIN_FRAGMENT = "One piece of a whole."
-	DESCRIBE.AIR_CONDITIONER = "Still not a fan. And neither is this!"
+	DESCRIBE.AIR_CONDITIONER = "Why would I ever want to spray mushrooms into the air?"
 	DESCRIBE.SPOREPACK = "It's going to spread it's spores on everything. Gross!"
+<<<<<<< Updated upstream
 	DESCRIBE.SALTPACK = "Melt all your troubles away."
 	
+=======
+	DESCRIBE.SALTPACK = "Saves time on shoveling."
+
+>>>>>>> Stashed changes
 	DESCRIBE.SCORPION = "Getting stung would be quite a bother!"
 	DESCRIBE.SCORPIONCARAPACE = "Slightly poisonous."
-	DESCRIBE.SCORPIONCARAPACECOOKED = "Times are hard, and food can be scarce."
-	DESCRIBE.HARDSHELLTACOS = "I'm unfamiliar with this cuisine."
-	
+	DESCRIBE.SCORPIONCARAPACECOOKED = "Not any more edible."
+	DESCRIBE.HARDSHELLTACOS = "Despite the carapace, it seems to be a lot better."
+
 	DESCRIBE.SKELETONMEAT = "Not even this will last."
 	DESCRIBE.CHIMP = DESCRIBE.MONKEY
 	DESCRIBE.SWILSON = "The shadows have taken hold of you!"
@@ -128,38 +133,38 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.CRITTERLAB_REAL = DESCRIBE.CRITTERLAB
 	DESCRIBE.CRITTERLAB_REAL_BROKEN = "Maybe as moon rock would fix it."
 	DESCRIBE.SLINGSHOTAMMO_FIRECRACKERS = DESCRIBE.FIRECRACKERS
-	DESCRIBE.CHARLIEPHONOGRAPH_100 = "It's that song again..."
-	DESCRIBE.BUGZAPPER = "An electric weapon? Maybe it was inevitable..."
-	
+	DESCRIBE.CHARLIEPHONOGRAPH_100 = "Have I heard this tune before?"
+	DESCRIBE.BUGZAPPER = "Helps with keeping bugs out of my hair."
+
 	--Swampyness
 	DESCRIBE.RICEPLANT = "Interesting, I haven't seen those around before..."
 	DESCRIBE.RICE = "Cooking it on it's own might be a waste of time."
-	DESCRIBE.RICE_COOKED = "A waste of time, and resources."
+	DESCRIBE.RICE_COOKED = "I might have rushed cooking this."
 	DESCRIBE.SEAFOODPAELLA = "Not bad!"
 	DESCRIBE.LICELOAF = "Not a huge waste of time, I suppose."
 	DESCRIBE.SUNGLASSES = "Tinted lenses, not bad!"
-	
+
 	DESCRIBE.STUMPLING = "The stumps are after me too?!"
 	DESCRIBE.BIRCHLING = DESCRIBE.STUMPLING
 	DESCRIBE.MOON_TEAR = "Is the moon... crying?"
 	DESCRIBE.SHADOW_TELEPORTER = "Oh, how you tempt me..."
 	DESCRIBE.POLLENMITEDEN = "They'll multiply in no time."
     DESCRIBE.POLLENMITES = "Don't you dare nest in my hair!"
-    DESCRIBE.SHADOW_CROWN = "This will help speed things up, shadow magic always comes at a cost."
+    DESCRIBE.SHADOW_CROWN = "Shadows have accelerated my movement and time."
     DESCRIBE.RNEGHOST = DESCRIBE.GHOST
 	DESCRIBE.TRAPDOORGRASS = DESCRIBE.GRASS
-	DESCRIBE.LUREPLAGUE_RAT = "Oh no, it's starting to take hold..."
-	DESCRIBE.MARSH_GRASS = "Huh, I thought these things were lost to time."
-	DESCRIBE.CURSED_ANTLER = "This will clean their clocks!"
+	DESCRIBE.LUREPLAGUE_RAT = "Oh botheration, what happened to you?"
+	DESCRIBE.MARSH_GRASS = "I thought these things were lost to time."
+	DESCRIBE.CURSED_ANTLER = "I could never really grab the antler off it before."
 	DESCRIBE.BERNIEBOX = "That's one mystery I'll leave to Willow." --unused, unedited
-	DESCRIBE.HOODED_FERN = "More plants. Neat."
+	DESCRIBE.HOODED_FERN = "A new take on ferns."
 	DESCRIBE.HOODEDWIDOW = "She's grown so quickly!"
 	DESCRIBE.GIANT_TREE = "These trees must be ancient, but how long have they been here?"
 	DESCRIBE.WIDOWSGRASP = "Might loosen up those cocoons."
 	DESCRIBE.WEBBEDCREATURE = "Preserved for later, I presume."
 	ANNOUNCE.WEBBEDCREATURE = "A waste of time, perhaps a tool is needed?"
-	DESCRIBE.SNAPDRAGON_BUDDY = "Hm, this guy might not be around for much longer."
-	DESCRIBE.SNAPDRAGON = "They'll evolve again, eventually."
+	DESCRIBE.SNAPDRAGON_BUDDY = "You stick out like a sore thumb."
+	DESCRIBE.SNAPDRAGON = "I feel like you aren't from around here."
 	DESCRIBE.SNAPPLANT = "Huh, you looked different in that other timeline."
 	DESCRIBE.WHISPERPOD = "More gardening? That will take so long!"
 	DESCRIBE.WHISPERPOD_NORMAL_GROUND =
@@ -168,14 +173,14 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 		GROWING = "That's okay, take your time.",
 	}
 	DESCRIBE.FRUITBAT = "This timeline is weird."
-	DESCRIBE.PITCHERPLANT = "Give it time, might prove useful."
-	DESCRIBE.APHID = "They have such a short lifespan, and they waste it eating twigs..."
+	DESCRIBE.PITCHERPLANT = "It's out of my grasp."
+	DESCRIBE.APHID = "They have such a short lifespan, yet they waste it eating twigs..."
 	DESCRIBE.GIANT_TREE_INFESTED = "Has it grown old, or is something else at play?"
 	DESCRIBE.GIANT_BLUEBERRY = "Disarmed, and ready to eat."
-	DESCRIBE.STEAMEDHAMS = "The future is now."
+	DESCRIBE.STEAMEDHAMS = "Would've been easier to take it from the real chefs."
 	DESCRIBE.BLUEBERRYPANCAKES = "Might bring me joy, if only for a moment."
 	DESCRIBE.SIMPSALAD = "This was a horrible waste of time."
-	DESCRIBE.BEEFALOWINGS = "It may leave my fingers sticky."
+	DESCRIBE.BEEFALOWINGS = "It was worth every tick to make."
 	ANNOUNCE.ANNOUNCE_ATTACH_BUFF_KNOCKBACKIMMUNE = "You won't knock me over, not in a million years!"
 	ANNOUNCE.ANNOUNCE_DETACH_BUFF_KNOCKBACKIMMUNE = "A million years may have been an exaggeration."
 	DESCRIBE.WIDOWSHEAD = "It's dead, right?"
@@ -183,14 +188,14 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.HOODED_MUSHTREE_MEDIUM = DESCRIBE.MUSHTREE_MEDIUM
 	DESCRIBE.HOODED_MUSHTREE_SMALL = DESCRIBE.MUSHTREE_SMALL
 	DESCRIBE.WATERMELON_LANTERN = DESCRIBE.PUMPKIN_LANTERN
-	
+
 		--Viperstuff Quotes
 	DESCRIBE.VIPERWORM = "It's been affected by the fuel."
 	DESCRIBE.VIPERFRUIT = "Tastes like shadows."
-	DESCRIBE.VIPERJAM = "A nice shadowy jam."
-	
-	
-	DESCRIBE.BLUEBERRYPLANT =         
+	DESCRIBE.VIPERJAM = "I'm tempted to eat it straight out of the jar."
+
+
+	DESCRIBE.BLUEBERRYPLANT =
 		{
             READY = "Careful now...",
 			FROZE = "It's clock has stopped.",
@@ -201,7 +206,7 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
             BROKEN = "This can't be the end. We'll fix this!",
             GENERIC = "It looks well-loved.",
         }
-	
+
 	DESCRIBE.BERNIE_ACTIVE =
         {
             GENERIC = "How incredible that this isn't even the strangest thing I've seen.",
@@ -215,17 +220,17 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.ANTIHISTAMINE = "May give some allergy protection, for a time."
     ANNOUNCE.CURSED_ITEM_EQUIP = "Ouch! It's not meant for uncursed hands."
 	DESCRIBE.VETSITEM = "It's not meant for uncursed hands."
-	DESCRIBE.SCREECHER_TRINKET = "It's from another timeline..."
+	DESCRIBE.SCREECHER_TRINKET = "It's from another timeline."
 	ACTIONFAIL.GIVE = {NOTNIGHT = "The moon needs to be out."}
-	
+
 	DESCRIBE.MAGMAHOUND = "Don't slobber on me!"
 	DESCRIBE.LIGHTNINGHOUND = "You can't control the lightning! Don't even try!"
-	DESCRIBE.SPOREHOUND = "This is why I hate mushrooms."
+	DESCRIBE.SPOREHOUND = "Oh botheration, the mushrooms got to the hounds as well."
 	DESCRIBE.GLACIALHOUND = "Ice cold."
-	DESCRIBE.RNESKELETON = "Age has caught up to them."
+	DESCRIBE.RNESKELETON = "You have overstayed your welcome."
 	DESCRIBE.RAT_WHIP = "Crack the whip!"
 	DESCRIBE.KLAUS_AMULET = "Double time!"
-	DESCRIBE.CRABCLAW = "You're about to get clocked!"
+	DESCRIBE.CRABCLAW = "I can finally put these gems to use!"
 	DESCRIBE.HAT_RATMASK = "Woodie did a good job with this one."
 
 	DESCRIBE.ORANGE_VOMIT = "Gross!"
@@ -235,45 +240,45 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.YELLOW_VOMIT = "Gross!"
 	DESCRIBE.PURPLE_VOMIT = "Gross!"
 	DESCRIBE.PALE_VOMIT = "Gross!"
-	
+
 	DESCRIBE.WALRUS_CAMP_EMPTY = DESCRIBE.WALRUS_CAMP.EMPTY
-	DESCRIBE.PIGKING_PIGGUARD = 
+	DESCRIBE.PIGKING_PIGGUARD =
 	{
 	GUARD = DESCRIBE.PIGMAN.GUARD,
 	WEREPIG = DESCRIBE.PIGMAN.WEREPIG,
 	}
 	DESCRIBE.PIGKING_PIGTORCH = DESCRIBE.PIGTORCH
-	
+
 	DESCRIBE.BIGHT = "A twisted creation!"
-	DESCRIBE.KNOOK = "How horrible..."
+	DESCRIBE.KNOOK = "How could they have ended up like this..?"
 	DESCRIBE.ROSHIP = "I'm shocked!"
-	
+
 	DESCRIBE.UM_PAWN = "Careful, it's a walking alarm clock."
 	DESCRIBE.UM_PAWN_NIGHTMARE = "A ticking time bomb!"
 
 	DESCRIBE.CAVE_ENTRANCE_SUNKDECID = DESCRIBE.CAVE_ENTRANCE
 	DESCRIBE.CAVE_ENTRANCE_OPEN_SUNKDECID = DESCRIBE.CAVE_ENTRANCE_OPEN
 	DESCRIBE.CAVE_EXIT_SUNKDECID= DESCRIBE.CAVE_EXIT
-	
+
 	-- Blowgun stuff
 	DESCRIBE.UNCOMPROMISING_BLOWGUN = DESCRIBE.BLOWDART_PIPE
 	DESCRIBE.BLOWGUNAMMO_TOOTH = DESCRIBE.BLOWDART_PIPE
 	DESCRIBE.BLOWGUNAMMO_FIRE = DESCRIBE.BLOWDART_FIRE
 	DESCRIBE.BLOWGUNAMMO_SLEEP = DESCRIBE.BLOWDART_SLEEP
 	DESCRIBE.BLOWGUNAMMO_ELECTRIC = DESCRIBE.BLOWDART_YELLOW
-	
+
 	DESCRIBE.ANCIENT_AMULET_RED = "It won't help me while I'm alive."
 	DESCRIBE.UM_BEAR_TRAP = "I don't like being stuck in one place."
 	DESCRIBE.UM_BEAR_TRAP_OLD = "I don't like being stuck in one place."
-	DESCRIBE.UM_BEAR_TRAP_EQUIPPABLE_TOOTH = "Springs, gears, and sharp teeth."
-	DESCRIBE.UM_BEAR_TRAP_EQUIPPABLE_GOLD = "Springs, gears, and sharp teeth."
+	DESCRIBE.UM_BEAR_TRAP_EQUIPPABLE_TOOTH = "Reminds me of clockwork."
+	DESCRIBE.UM_BEAR_TRAP_EQUIPPABLE_GOLD = "Springs, gears, and sharp teeth. It really is clockwork."
 	DESCRIBE.SKULLCHEST_CHILD = "The insides can exist at multiple points in time!"
-	
+
 	DESCRIBE.SLOBBERLOBBER = "Rude, and crude!"
 	DESCRIBE.GORE_HORN_HAT = "It turns speed into shadows!"
 	DESCRIBE.BEARGERCLAW = "It still has a pulse..."
 	DESCRIBE.FEATHER_FROCK = "Time flies, and so will I!"
-	
+
 	DESCRIBE.REDGEM_CRACKED = DESCRIBE.REDGEM.."\n...It's cut is wrong! Only the Crab Claw can channel it's power."
 	DESCRIBE.BLUEGEM_CRACKED = DESCRIBE.BLUEGEM.."\n...It's cut is wrong! Only the Crab Claw can channel it's power."
 	DESCRIBE.ORANGEGEM_CRACKED = DESCRIBE.ORANGEGEM.."\n...It's cut is wrong! Only the Crab Claw can channel it's power."
@@ -281,7 +286,7 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.YELLOWGEM_CRACKED = DESCRIBE.YELLOWGEM.."\n...It's cut is wrong! Only the Crab Claw can channel it's power."
 	DESCRIBE.PURPLEGEM_CRACKED = DESCRIBE.PURPLEGEM.."\n...It's cut is wrong! Only the Crab Claw can channel it's power."
 	DESCRIBE.OPALPRECIOUSGEM_CRACKED = DESCRIBE.OPALPRECIOUSGEM.."\n...It's cut is wrong! Only the Crab Claw can channel it's power."
-	
+
 	DESCRIBE.RED_MUSHED_ROOM = "It's all mushy now..."
 	DESCRIBE.GREEN_MUSHED_ROOM = "It's all mushy now..."
 	DESCRIBE.BLUE_MUSHED_ROOM = "It's all mushy now..."
@@ -296,28 +301,28 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	ANNOUNCE.ANNOUNCE_ATTACH_BUFF_HYPERCOURAGE = "Things don't seem so scary, all of a sudden..."
 	ANNOUNCE.ANNOUNCE_DETACH_BUFF_HYPERCOURAGE = "Now I'm left with a bit of a headache."
 	--StantonStuff
-	
+
 	DESCRIBE.ARMORLAVAE = DESCRIBE.LAVAE
-	
+
 	DESCRIBE.THEATERCORN = "If only we had some moving picture shows to go with it."
 	DESCRIBE.DEERCLOPS_BARRIER = "I can't wait for it to melt!"
-	
 
-	DESCRIBE.MOONMAW_DRAGONFLY = "The magma has hardened into a sharp glass!"
+
+	DESCRIBE.MOONMAW_DRAGONFLY = "That fly became a lunatic!"
 	DESCRIBE.MOONMAW_LAVAE = "Crystaline entities!"
 	DESCRIBE.SNAPPERTURTLE = "It will live longer than me, naturally."
 	DESCRIBE.SNAPPERTURTLEBABY = "Time is precious. Take my advice, use it wisely."
-	DESCRIBE.SNAPPERTURTLENEST = "A damp nest for the the turtles."
+	DESCRIBE.SNAPPERTURTLENEST = "A damp nest for the turtles."
 	DESCRIBE.GLASS_SCALES = "Radiant, and quite sharp."
 	DESCRIBE.MOONGLASS_GEODE = "The charged glass won't last long once broken down."
 	DESCRIBE.ARMOR_GLASSMAIL = "Keep the pace!"
 	DESCRIBE.ARMOR_GLASSMAIL_SHARDS = "I'm the center point, they're my clock hands!"
 	DESCRIBE.MOONMAW_GLASSSHARDS_RING = DESCRIBE.ARMOR_GLASSMAIL_SHARDS
-	DESCRIBE.MOONMAW_GLASSSHARDS = DESCRIBE.ARMOR_GLASSMAIL_SHARDS	
+	DESCRIBE.MOONMAW_GLASSSHARDS = DESCRIBE.ARMOR_GLASSMAIL_SHARDS
 	DESCRIBE.MOONMAW_LAVAE_RING = DESCRIBE.MOONMAW_LAVAE
-	
+
 	DESCRIBE.MUTATOR_TRAPDOOR = DESCRIBE.MUTATOR_WARRIOR
-	
+
 	DESCRIBE.WOODPECKER = "Takes patience to do what they do."
 	DESCRIBE.SNOTROAST = "Close your eyes and pretend it doesn't takes like snot."
 	ANNOUNCE.ANNOUNCE_ATTACH_BUFF_LARGEHUNGERSLOW = "I think my appetite has slowed."
@@ -325,8 +330,8 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.BOOK_RAIN = "It tampers with the timestream of the local clouds."
 	DESCRIBE.FLORAL_BANDAGE = "I don't have the time to mess with that."
 	DESCRIBE.DORMANT_RAIN_HORN = "How did that crab bring this here?"
-	DESCRIBE.RAIN_HORN = "It was warped when it was pulled across the timestream."	
-	DESCRIBE.DRIFTWOODFISHINGROD = "Something that saves time. Much better."
+	DESCRIBE.RAIN_HORN = "It was warped when it was pulled across the timestream."
+	DESCRIBE.DRIFTWOODFISHINGROD = "A much more stable way of fishing."
 
     ANNOUNCE.ANNOUNCE_RATSNIFFER_ITEMS = {
         LEVEL_1 = "I think it's time to clean up!",
@@ -342,6 +347,7 @@ ACTIONFAIL = GLOBAL.STRINGS.CHARACTERS.WANDA.ACTIONFAIL
 	DESCRIBE.PIED_PIPER_FLUTE = "Maybe it'd be worth the time to play."
 	DESCRIBE.UNCOMPROMISING_PACKRAT = "We must focus on that one!"
 	DESCRIBE.WINONA_TOOLBOX = ""
+	DESCRIBE.WINONA_TOOLBOX = "fuck you ato i mean i love you except not really actually hahaahahahahaha platonically haha (this is a co written message)"
 	DESCRIBE.OCEAN_SPEAKER = "I remember these! I can't quite... remember what they do though..."
 	--DESCRIBE.UM_SIREN = "Oh botheration! Its YOU again."
 
