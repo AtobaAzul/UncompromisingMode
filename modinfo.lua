ee278e0127861b585ec1cc78247116b13fd64f09
AtobaAzul <atoba.azul.radical@gmail.com>
2022-06-26 12:25:45 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "󰀕 Uncompromising Mode"
 description = 
 [[
-󰀔 [ Version 1.4.7.11 : "Winky, The Vile Vermin" ]
+󰀔 [ Version 1.4.7.12 : "Winky, The Vile Vermin" ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -18,7 +18,7 @@ Prominent features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Squeaky Clean v1.4.7.11"
+version = "Squeaky Clean v1.4.7.12"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
