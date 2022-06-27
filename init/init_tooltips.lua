bcef932c1f1327550452322517c200050066f6c9
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-06-27 11:08:05 -0400

    live version of tooltip sleepy crash, also removed fishing net inv icon

--- init/init_tooltips.lua
+++ init/init_tooltips.lua
@@ -109,7 +109,7 @@ else
 	TOOLTIP.MOLEHAT = "- Grants protection against Sand Storms when worn."
 end
 
-if GetModConfigData("hayfever") then
+if TUNING.DSTU.HAYFEVER then
 	TOOLTIP.GASMASK = TOOLTIP.GASMASK.."\n- Prevents Hayfever while worn."
 	TOOLTIP.PLAGUEMASK = TOOLTIP.PLAGUEMASK.."\n- Prevents Hayfever while worn."
 	TOOLTIP.BEEHAT = "- Stings won't cause allergic reactions."
@@ -126,11 +126,19 @@ else
 end
 
 if TUNING.DSTU.SLEEPINGBUFF > 1 then
-	TOOLTIP.BEDROLL_STRAW = TOOLTIP.BEDROLL_STRAW.."\n- Faster healing rate."
-	TOOLTIP.BEDROLL_FURRY = TOOLTIP.BEDROLL_FURRY.."\n- Faster healing rate."
-	TOOLTIP.TENT = TOOLTIP.TENT.."\n- Faster healing rate."
-	TOOLTIP.SIESTAHUT = TOOLTIP.SIESTAHUT.."\n- Faster healing rate."
-    TOOLTIP.PORTABLETENT_ITEM = TOOLTIP.PORTABLETENT_ITEM.."\n- Faster healing rate."
+	if TUNING.DSTU.GOTOBED then
+		TOOLTIP.BEDROLL_STRAW = TOOLTIP.BEDROLL_STRAW.."\n- Faster healing rate."
+		TOOLTIP.BEDROLL_FURRY = TOOLTIP.BEDROLL_FURRY.."\n- Faster healing rate."
+		TOOLTIP.TENT = TOOLTIP.TENT.."\n- Faster healing rate."
+		TOOLTIP.SIESTAHUT = TOOLTIP.SIESTAHUT.."\n- Faster healing rate."
+		TOOLTIP.PORTABLETENT_ITEM = TOOLTIP.PORTABLETENT_ITEM.."\n- Faster healing rate."
+	else
+		TOOLTIP.BEDROLL_STRAW = "- Faster healing rate."
+		TOOLTIP.BEDROLL_FURRY = "- Faster healing rate."
+		TOOLTIP.TENT = "- Faster healing rate."
+		TOOLTIP.SIESTAHUT = "- Faster healing rate."
+		TOOLTIP.PORTABLETENT_ITEM = "- Faster healing rate."
+	end
 end
 
 if TUNING.DSTU.POCKET_POWERTRIP ~= 0 then
