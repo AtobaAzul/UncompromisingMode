1a2d088f25efb863d924a251db362c364a98c4f9
AtobaAzul <atoba.azul.radical@gmail.com>
2022-08-15 23:06:48 -0300

    maybe this fixes it??? Use GetMemoryCount instead of GetFoodMult. for warly's bonus desc.

--- init/init_bonusdescriptors.lua
+++ init/init_bonusdescriptors.lua
@@ -118,7 +118,7 @@ GLOBAL.GetDescription = function(inst, item, ...)
 		ret = ret .."\n".. item.vetstring
 	end
 	
-	if prefab and item and inst:HasTag("masterchef") and inst.components.foodmemory ~= nil and inst.components.foodmemory:GetFoodMultiplier(prefab) ~= 1 then
+	if prefab and item and inst:HasTag("masterchef") and inst.components.foodmemory ~= nil and inst.components.foodmemory:GetMemoryCount(prefab) > 0 then
 		ret = ret .."\n".. "I would prefer something a little more... varied."
 	end
 	
