40d0dcdc52d438d75320af3f27846932276af03d
Scrimbles <40772985+Shodance@users.noreply.github.com>
2022-08-16 12:39:25 -0400

    Critter lab now repairs into the original prefab, fixing issues and missing quotes LIVE

--- postinit/prefabs/critterlab.lua
+++ postinit/prefabs/critterlab.lua
@@ -2,14 +2,14 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------
 local function onnear(inst, target)
-    if inst.components.childspawner ~= nil then
+    if inst.components.childspawner ~= nil and inst.ambush == nil then
         SpawnPrefab("collapse_small").Transform:SetPosition(inst.Transform:GetWorldPosition())
         inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/explo")
         SpawnPrefab("collapse_small").Transform:SetPosition(inst.Transform:GetWorldPosition())
         inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/explo")
         SpawnPrefab("collapse_small").Transform:SetPosition(inst.Transform:GetWorldPosition())
         inst.SoundEmitter:PlaySound("dontstarve/creatures/rook/explo")
-        SpawnPrefab("critterlab_real_broken").Transform:SetPosition(inst.Transform:GetWorldPosition())
+        SpawnPrefab("critterlab_broken").Transform:SetPosition(inst.Transform:GetWorldPosition())
         inst.components.childspawner:ReleaseAllChildren(target, "mutatedhound")
 		inst:DoTaskInTime(0, function() inst:Remove() end)
     end
@@ -29,5 +29,45 @@ env.AddPrefabPostInit("critterlab", function (inst)
     inst.components.playerprox:SetDist(4, 6) --set specific values
     inst.components.playerprox:SetOnPlayerNear(onnear)
     inst.components.playerprox:SetPlayerAliveMode(inst.components.playerprox.AliveModes.AliveOnly)
+	
+	local _OldOnSave = inst.OnSave
+	local _OldOnLoad = inst.OnLoad
+	
+	if inst.OnSave ~= nil then
+		_OldOnSave = inst.OnSave
+	end
+	
+	if inst.OnLoad ~= nil then
+		_OldOnLoad = inst.OnLoad
+	end
+	
+	local function OnSave(inst, data)
+		if inst.ambush ~= nil then
+			data.ambush = inst.ambush
+		end
+		
+		if _OldOnSave ~= nil then
+			_OldOnSave(inst, data)
+		end
+	end
+	
+    inst.OnSave = OnSave
+	
+	local function OnLoad(inst, data)
+		if data ~= nil then
+			if data.ambush then
+				inst.ambush = data.ambush
+			end
+		end
+		
+		if _OldOnLoad ~= nil then
+			_OldOnLoad(inst, data)
+		end
+	end
+	
+    inst.OnLoad = OnLoad
 
+	if inst.ambush == nil then
+		inst.ambush = true
+	end
 end)
