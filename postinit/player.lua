b5fa970e042f1a6823db6ea8d24539643b6afeb4
AtobaAzul <atoba.azul.radical@gmail.com>
2022-08-26 16:52:02 -0300

    restored charging code in live

--- postinit/player.lua
+++ postinit/player.lua
@@ -16,4 +16,98 @@ env.AddPlayerPostInit(function(inst)
             inst.components.debuffable:RemoveDebuff("buff_vetcurse")
         end -- help I can't get this stupid thing to work!!
     end
+
+    local function ChargeItem(item)
+        if item ~= nil then
+            if item.components.fueled ~= nil then
+                local percent = item.components.fueled:GetPercent()
+                local refuelnumber = 0
+
+                if percent + 0.33 > 1 then
+                    refuelnumber = 1
+                else
+                    refuelnumber = percent + 0.33
+                end
+
+                item.components.fueled:SetPercent(refuelnumber)
+            elseif item.components.finiteuses ~= nil then
+                local percent = item.components.finiteuses:GetPercent()
+                local refuelnumber = 0
+
+                if percent + 0.33 > 1 then
+                    refuelnumber = 1
+                else
+                    refuelnumber = percent + 0.33
+                end
+
+                item.components.finiteuses:SetPercent(refuelnumber)
+            end
+        end
+    end
+
+    local function OnChargeFromBattery(inst, battery)
+        local item = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.HANDS)
+
+        if item == nil or (item ~= nil and not item:HasTag("electricaltool")) or (item ~= nil and ((item.components.finiteuses ~= nil and item.components.finiteuses:GetPercent() == 1) or (item.components.fueled ~= nil and item.components.fueled:GetPercent() >= 0.99))) then
+            item = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.HEAD)
+        end
+
+        if item == nil or (item ~= nil and not item:HasTag("electricaltool")) or (item ~= nil and ((item.components.finiteuses ~= nil and item.components.finiteuses:GetPercent() == 1) or (item.components.fueled ~= nil and item.components.fueled:GetPercent() >= 0.99))) then
+            item = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.BODY)
+        end
+
+        if (item ~= nil and ((item.components.finiteuses ~= nil and item.components.finiteuses:GetPercent() == 1) or (item.components.fueled ~= nil and item.components.fueled:GetPercent() > 0.99))) then
+            return false, "CHARGE_FULL"
+        end
+
+        if inst.components.upgrademoduleowner == nil then
+            if (item ~= nil and item.components.finiteuses ~= nil and item.components.finiteuses:GetPercent() == 1) or (item ~= nil and item.components.fueld ~= nil and item.components.fueled:GetPercent() >= 0.99) then
+                return false, "CHARGE_FULL"
+            else
+                if item ~= nil and item:HasTag("electricaltool") then
+                    ChargeItem(item)
+                    if not inst.components.inventory:IsInsulated() then
+                        inst.sg:GoToState("electrocute")
+                        inst.components.health:DoDelta(-TUNING.HEALING_SMALL, false, "lightning")
+                        inst.components.sanity:DoDelta(-TUNING.SANITY_SMALL)
+                        if inst.components.talker ~= nil then
+                            inst:DoTaskInTime(1, inst.components.talker:Say(GetString(inst, "ANNOUNCE_CHARGE_SUCCESS_ELECTROCUTED")))
+                        end
+                    else
+                        if inst.components.talker ~= nil then
+                            inst:DoTaskInTime(1, inst.components.talker:Say(GetString(inst, "ANNOUNCE_CHARGE_SUCCESS_INSULATED")))
+                        end
+                    end
+                    return true
+                end
+            end
+        else
+            if ((item ~= nil and item.components.finiteuses ~= nil and item.components.finiteuses:GetPercent() == 1) or (item ~= nil and item.components.fueld ~= nil and item.components.fueled:GetPercent() >= 0.995)) and inst.components.upgrademoduleowner:ChargeIsMaxed() or not (item ~= nil and item:HasTag("electricaltool")) then
+                return false, "CHARGE_FULL"
+            else
+                if item ~= nil and item:HasTag("electricaltool") then
+                    ChargeItem(item)
+                    if not inst.components.upgrademoduleowner:ChargeIsMaxed() then
+                        inst.components.upgrademoduleowner:AddCharge(1)
+                    end
+                    if not inst.components.inventory:IsInsulated() then
+                        inst.sg:GoToState("electrocute")
+                        inst.components.health:DoDelta(-TUNING.HEALING_SMALL, false, "lightning")
+                        inst.components.sanity:DoDelta(-TUNING.SANITY_SMALL)
+                        if inst.components.talker ~= nil then
+                            inst:DoTaskInTime(1, inst.components.talker:Say(GetString(inst, "ANNOUNCE_CHARGE_SUCCESS_ELECTROCUTED")))
+                        end
+                    else
+                        if inst.components.talker ~= nil then
+                            inst:DoTaskInTime(1, inst.components.talker:Say(GetString(inst, "ANNOUNCE_CHARGE_SUCCESS_INSULATED")))
+                        end
+                    end
+                    return true
+                end
+            end
+        end
+    end
+
+    inst:AddComponent("batteryuser") -- just the component by itself doesn't do anything
+    inst.components.batteryuser.onbatteryused = OnChargeFromBattery
 end)
