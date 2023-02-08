c1dd010968134b0b4ba37a912c8e8fafdeb5b1a1
atoba.azul.radical <atoba.azul.radical@gmail.com>
2023-02-08 13:47:08 -0300

    Fixed `fish` being a boolean value  ocassionally (fixed a crash when netting ocean debris)

--- postinit/components/fishingnet.lua
+++ postinit/components/fishingnet.lua
@@ -35,12 +35,13 @@ env.AddComponentPostInit("fishingnetvisualizer", function(self)
 			local fishies = TheSim:FindEntities(my_x,my_y,my_z, self.collect_radius, {"oceanfishable"})
 			for k, v in pairs(fishies) do
 				local fishdef = v.fish_def ~= nil and v.fish_def.prefab ~= nil and v.fish_def.prefab or nil
-				local fish = fishdef ~= nil and SpawnPrefab(fishdef.."_inv")
+				local fish = fishdef ~= nil and SpawnPrefab(fishdef.."_inv") or nil
 				
 				if fish == nil then
-					fish = fishdef ~= nil and SpawnPrefab(fishdef.."_land")
+					fish = fishdef ~= nil and SpawnPrefab(fishdef.."_land") or nil
 				end
 					
+				--how the FUCK does a 
 				if fish == nil then
 					return
 				elseif fish ~= nil and k < 3 then
