a45c9f3d6afec913129016b8d5d0cd1037e8c4c8
Scrimbles <40772985+Shodance@users.noreply.github.com>
2023-03-09 14:52:54 -0500

    I changed the mod version to Face Your Fears v1.0.0 because its the first version of  the face your fears update

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "󰀕 Uncompromising Mode"
 description =
 [[
-󰀔 [ Version 1.6: "Face Your Fears" ]
+󰀔 [ Version 1.0.0: "Face Your Fears" ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -15,7 +15,7 @@ Latest update features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Face Your Fears v1.6"
+version = "Face Your Fears v1.0.0"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
