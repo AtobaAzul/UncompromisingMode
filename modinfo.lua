464693848e872864345c7b99185566946663ec90
atoba.azul.radical <atoba.azul.radical@gmail.com>
2023-03-19 13:20:12 -0300

    ver

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "󰀕 Uncompromising Mode"
 description =
 [[
-󰀔 [ Version 1.0.10: "Face Your Fears" ]
+󰀔 [ Version 1.0.10.1: "Face Your Fears" ]
 
 Uncompromising Mode increases the risk and reward for those who have mastered Don't Starve Together.
 
@@ -15,7 +15,7 @@ Latest update features:
 
 author = "󰀈 The Uncomp Dev Team 󰀈"
 
-version = "Face Your Fears v1.0.10"
+version = "Face Your Fears v1.0.10.1"
 
 forumthread = "/topic/111892-announcement-uncompromising-mode/"
 
