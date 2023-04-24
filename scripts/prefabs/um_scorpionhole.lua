3f1b985a27c95a317efe65a309df86031d52c7ed
Scrimbles <40772985+Shodance@users.noreply.github.com>
2023-04-24 02:31:43 -0400

    axes scorpion hole fix

--- scripts/prefabs/um_scorpionhole.lua
+++ scripts/prefabs/um_scorpionhole.lua
@@ -30,35 +30,6 @@ local function set_stage(inst, workleft, play_grow_sound)
     inst._stage = new_stage
 end
 
----------------------------------------------------------------------------
----- WORKABLE REGENERATION TASK MANAGEMENT ----
----------------------------------------------------------------------------
-
-local function stop_regen_task(inst)
-    if inst._regen_task ~= nil then
-        inst._regen_task:Cancel()
-        inst._regen_task = nil
-    end
-end
-
-local function try_work_regen(inst)
-    if inst.components.workable ~= nil and inst.components.workable.workleft < TUNING.MOONSPIDERDEN_WORK then
-        local new_work_amount = inst.components.workable.workleft + 1
-        set_stage(inst, new_work_amount, true)
-        inst.components.workable:SetWorkLeft(new_work_amount)
-
-        -- If we've regenerated back to our max work amount, end the task. We'll start it up again the next time we get worked on.
-        if new_work_amount == TUNING.MOONSPIDERDEN_WORK then
-            stop_regen_task(inst)
-        end
-    end
-end
-
-local function start_regen_task(inst)
-    if inst._regen_task == nil then
-        inst._regen_task = inst:DoPeriodicTask(TUNING.MOONSPIDERDEN_WORK_REGENTIME, try_work_regen)
-    end
-end
 
 ---------------------------------------------------------------------------
 
@@ -111,12 +82,9 @@ local function spawner_onworked(inst, worker, workleft)
         local pos = inst:GetPosition()
         SpawnPrefab("rock_break_fx").Transform:SetPosition(pos:Get())
         inst.components.lootdropper:DropLoot(pos)
-
-        stop_regen_task(inst)
         inst:Remove()
     else
         set_stage(inst, workleft, false)
-        start_regen_task(inst)
     end
 
     if inst.components.childspawner ~= nil then
@@ -127,70 +95,11 @@ end
 
 local function on_workable_load(inst)
     set_stage(inst, inst.components.workable.workleft, false)
-
-    if inst.components.workable.workleft < TUNING.MOONSPIDERDEN_WORK then
-        start_regen_task(inst)
-    end
 end
 
 ---------------------------------------------------------------------------
 
-local function on_save(inst, data)
-    if inst._sleep_start_time ~= nil and (inst.components.workable.workleft < inst.components.workable.maxwork) then
-        local time_since_sleep = GetTime() - inst._sleep_start_time
 
-        -- Do 'proper' rounding, so that we're closer to the correct amount of work than we would be
-        -- just from a floor or ceiling.
-        local work_regen_during_sleep = math.floor((time_since_sleep / TUNING.MOONSPIDERDEN_WORK_REGENTIME) + 0.5)
-
-        if work_regen_during_sleep > 0 then
-            data.sleeping_work_regen = work_regen_during_sleep
-        end
-    end
-end
-
-local function on_load(inst, data)
-    if data ~= nil and data.sleeping_work_regen ~= nil then
-        local new_work_amount = math.min(inst.components.workable.maxwork, inst.components.workable.workleft + data.sleeping_work_regen)
-        inst.components.workable:SetWorkLeft(new_work_amount)
-        set_stage(inst, new_work_amount, true)
-
-        -- If we've regenerated back to our max work amount, end the regeneration task (we may have started it in on_workable_load)
-        -- We'll start it up again the next time we get worked on.
-        if new_work_amount == TUNING.MOONSPIDERDEN_WORK then
-            stop_regen_task(inst)
-        end
-    end
-end
-
-local function on_sleep(inst)
-
-end
-
-local function on_wake(inst)
-
-    if inst._sleep_start_time ~= nil and inst.components.workable ~= nil and (inst.components.workable.workleft < inst.components.workable.maxwork) then
-        local time_since_sleep = GetTime() - inst._sleep_start_time
-
-        -- Do 'proper' rounding, so that we're closer to the correct amount of work than we would be
-        -- just from a floor or ceiling.
-        local work_regen_during_sleep = math.floor((time_since_sleep / TUNING.MOONSPIDERDEN_WORK_REGENTIME) + 0.5)
-
-        if work_regen_during_sleep > 0 then
-            local new_work_amount = math.min(inst.components.workable.maxwork, inst.components.workable.workleft + work_regen_during_sleep)
-            inst.components.workable:SetWorkLeft(new_work_amount)
-
-            set_stage(inst, new_work_amount, true)
-        end
-
-        -- If we still haven't hit our maxwork, we need to restart the regen task.
-        if inst.components.workable.workleft < inst.components.workable.maxwork then
-            start_regen_task(inst)
-        end
-    end
-
-    inst._sleep_start_time = nil
-end
 
 local function OnPreLoad(inst, data)
 
@@ -231,7 +140,7 @@ local function OnInit(inst)
 end
 
 local function OnPlayerNear(inst)
-	inst.components.childspawner:SetSpawnPeriod(TUNING.MOONSPIDERDEN_RELEASE_TIME/20)
+	inst.components.childspawner:SetSpawnPeriod(60)
 	inst.components.childspawner:StartSpawning() --Bypass Day Stuff, home is being trespassed...
 	inst.components.childspawner.timetonextspawn = math.random(1,3)
 end
@@ -240,7 +149,7 @@ local function OnPlayerFar(inst)
 	local x,y,z = inst.Transform:GetWorldPosition()
 	local players = #TheSim:FindEntities(x,y,z,15,{"player"},{"playerghost"})
 	if players == 0 then
-		inst.components.childspawner:SetSpawnPeriod(TUNING.MOONSPIDERDEN_RELEASE_TIME)
+		inst.components.childspawner:SetSpawnPeriod(60)
 		if TheWorld.state.iscaveday then
 			StopSpawning(inst)
 		end
@@ -293,8 +202,8 @@ local function scorpionhole_fn()
     inst.components.workable:SetOnLoadFn(on_workable_load)
 
     inst:AddComponent("childspawner")
-    inst.components.childspawner:SetRegenPeriod(TUNING.MOONSPIDERDEN_SPIDER_REGENTIME)
-    inst.components.childspawner:SetSpawnPeriod(TUNING.MOONSPIDERDEN_RELEASE_TIME)
+    inst.components.childspawner:SetRegenPeriod(180)
+    inst.components.childspawner:SetSpawnPeriod(15)
     inst.components.childspawner:SetGoHomeFn(OnGoHome)
 	
 	
@@ -315,12 +224,7 @@ local function scorpionhole_fn()
     set_stage(inst, TUNING.MOONSPIDERDEN_WORK, false)
 
     inst.components.childspawner:StartSpawning()
-
-    inst.OnEntitySleep = on_sleep
-    inst.OnEntityWake = on_wake
-
-    inst.OnSave = on_save
-    inst.OnLoad = on_load
+	
     inst.OnPreLoad = OnPreLoad
 
     inst.SummonChildren = SummonChildren
