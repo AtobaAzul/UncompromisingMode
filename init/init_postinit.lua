79511c76fbc584206f9afaf8d98b7412360fd550
atoba.azul.radical <atoba.azul.radical@gmail.com>
2023-05-05 10:35:27 -0300

    fix formatting in init/init_postinit

--- init/init_postinit.lua
+++ init/init_postinit.lua
@@ -1,75 +1,201 @@
 -- Update this list when adding files
 local component_post = {
-    "groundpounder", "propagator", "moisture", "weapon", --	"hunter",
-    "kramped", "explosiveresist", "crop", "wildfires", --	"beargerspawner",
-    "workable", "sleepingbaguser", "hounded", "sleeper", "fueled", "perishable",
+    "groundpounder",
+    "propagator",
+    "moisture",
+    "weapon", --	"hunter",
+    "kramped",
+    "explosiveresist",
+    "crop",
+    "wildfires", --	"beargerspawner",
+    "workable",
+    "sleepingbaguser",
+    "hounded",
+    "sleeper",
+    "fueled",
+    "perishable",
     --	"carnivalevent",
-    "lootdropper", "ambientsound", "foodaffinity", "eater", "edible",
+    "lootdropper",
+    "ambientsound",
+    "foodaffinity",
+    "eater",
+    "edible",
     "inventory", -- This is an attempt at manually fixing an issue when people are checked for insulation. -scrimbles
-    "weighable", "messagebottlemanager", "fishingnet", "boatleak", -- for custom boat patches.
-    "wisecracker", "boatphysics", "map", "playerspawner", --	"drownable",
-    "combat", "combat_replica", "hullhealth", "health", --	"spellbook"
-    "finiteuses", "piratespawner", "repairable", "sewing", "container"
+    "weighable",
+    "messagebottlemanager",
+    "fishingnet",
+    "boatleak", -- for custom boat patches.
+    "wisecracker",
+    "boatphysics",
+    "map",
+    "playerspawner", --	"drownable",
+    "combat",
+    "combat_replica",
+    "hullhealth",
+    "health", --	"spellbook"
+    "finiteuses",
+    "piratespawner",
+    "repairable",
+    "sewing",
+    "container"
 }
 
 local prefab_post = {
-    "wardrobe", "shieldofcthulu", "clockworks", "flingobalance", --	"chester",
+    "wardrobe",
+    "shieldofcthulu",
+    "clockworks",
+    "flingobalance", --	"chester",
     "mushlight", --	"toadstool_cap", Moved to init_uncompromising_mod.lua
-    "amulet", "cave_entrance_open", "catcoon", "icehound", "firehound",
+    "amulet",
+    "cave_entrance_open",
+    "catcoon",
+    "icehound",
+    "firehound",
     "fishmeats", -- fish meat now dries into fish jerky
-    "forest", "cave", "world", "antlion", "minifan", "spider", "hound", "ash",
-    "bunnyman", "walls", "infestables", "foodbuffs", "mutatedhound", "skeleton",
+    "forest",
+    "cave",
+    "world",
+    "antlion",
+    "minifan",
+    "spider",
+    "hound",
+    "ash",
+    "bunnyman",
+    "walls",
+    "infestables",
+    "foodbuffs",
+    "mutatedhound",
+    "skeleton",
     --	"shadowcreature",
     "berrybush", --	"papyrus",
-    "sporecloud", "featherhat", --	"malbatross",
-    "mushrooms", "rock_ice", "toadstool", "oasislake", --	"shadowcreature",
-    "lureplant", "spiderden", "stafflights", "armor_ruins", "sweatervest",
-    "fans", "skeletonhat", --	"rock_avocado_fruit_sprout_sapling",
-    "icepack", "heatrock", "dragonfly_spawner", "rneghostfire", "monkey",
-    "batcave", "rain", "molehat", "klaus", "mosquito", "armor_bramble",
+    "sporecloud",
+    "featherhat", --	"malbatross",
+    "mushrooms",
+    "rock_ice",
+    "toadstool",
+    "oasislake", --	"shadowcreature",
+    "lureplant",
+    "spiderden",
+    "stafflights",
+    "armor_ruins",
+    "sweatervest",
+    "fans",
+    "skeletonhat", --	"rock_avocado_fruit_sprout_sapling",
+    "icepack",
+    "heatrock",
+    "dragonfly_spawner",
+    "rneghostfire",
+    "monkey",
+    "batcave",
+    "rain",
+    "molehat",
+    "klaus",
+    "mosquito",
+    "armor_bramble",
     --	"woby",
-    "cave_network", "glasscutter", "critterlab", "wobster", "trinkets", -- This is for the grave mound cc trinkets
+    "cave_network",
+    "glasscutter",
+    "critterlab",
+    "wobster",
+    "trinkets", -- This is for the grave mound cc trinkets
     "trap", -- prevents traps and rabbits from 'sleeping' off screen
-    "moonbase", "koalas", "pumpkin_lantern", "piggyback", "nightlight",
-    "armor_sanity", "tophat", "tophatreduction", "ruinsstatues", "moondial",
+    "moonbase",
+    "koalas",
+    "pumpkin_lantern",
+    "piggyback",
+    "nightlight",
+    "armor_sanity",
+    "tophat",
+    "tophatreduction",
+    "ruinsstatues",
+    "moondial",
     --	"deciduoustrees",
-    "pktrades", "pigking", "marblebean", "reviver", "krampus_sack",
+    "pktrades",
+    "pigking",
+    "marblebean",
+    "reviver",
+    "krampus_sack",
     --	"bundle",
-    "eyebrella", "birds", "tonichandlers", "houndwarning", --	"carnival_host",
-    "spider_whistle", "spider_healer", "siestahut", "bees", "farmplants",
-    "rainhat", "darts", "terrarium", "bandage", "grassgekko", "minotaur_drops",
-    "frog", "waterplant", --	"grassgator",
-    "alterguardian", "seasonal_shoals", "petals", "lantern", "minerhat",
+    "eyebrella",
+    "birds",
+    "tonichandlers",
+    "houndwarning", --	"carnival_host",
+    "spider_whistle",
+    "spider_healer",
+    "siestahut",
+    "bees",
+    "farmplants",
+    "rainhat",
+    "darts",
+    "terrarium",
+    "bandage",
+    "grassgekko",
+    "minotaur_drops",
+    "frog",
+    "waterplant", --	"grassgator",
+    "alterguardian",
+    "seasonal_shoals",
+    "petals",
+    "lantern",
+    "minerhat",
     "shark",
     --	"shadowchesspieces", Only bring this back when we make the fight cool
-    "fertilizer", "stinger", "boat_pirate", "trident", -- for giving the leak a cause.
-    "boat_bumpers", "slurtle_shellpieces", "sludge_fueled", "nightsword",
-    "bigshadowtentacle", "mast", "multitool_axe_pickaxe", "staffs", -- generic staffs.
-    "stalker", "sacred_chest", "soil_amender", "vetcurse_drops", "charcoal",
-    "mermhat", "wptags", "inventoryitem_classified", "cannonballs",
+    "fertilizer",
+    "stinger",
+    "boat_pirate",
+    "trident", -- for giving the leak a cause.
+    "boat_bumpers",
+    "slurtle_shellpieces",
+    "sludge_fueled",
+    --"nightsword",
+    "bigshadowtentacle",
+    "mast",
+    "multitool_axe_pickaxe",
+    "staffs", -- generic staffs.
+    "stalker",
+    "sacred_chest",
+    "soil_amender",
+    "vetcurse_drops",
+    "charcoal",
+    "mermhat",
+    "wptags",
+    "inventoryitem_classified",
+    "cannonballs",
     -- "renameable_items",
     "archive_centipede"
 
 }
 
 local stategraph_post = {
-    "wilson", "wilson_client", "spider", "frog", --	"wobysmall",
+    "wilson",
+    "wilson_client",
+    "spider",
+    "frog", --	"wobysmall",
     --	"shadowcreature",
     "stalker_minion", --	"merm",
-    "carnival_host", "catcoon"
+    "carnival_host",
+    "catcoon"
 }
 
 local class_post = {
     --	"components/inventoryitem_replica",
     --	"screens/playerhud",
     "widgets/itemtile", --	"widgets/hoverer",
-    "widgets/moisturemeter", "widgets/controls", "widgets/craftslot",
+    "widgets/moisturemeter",
+    "widgets/controls",
+    "widgets/craftslot",
     "widgets/bloodover"
 }
 
 local brain_post = {
     --	"hound",
-    "frog", "nofirepanic", "chester", "mossling", "perd", "catcoon", "deer",
+    "frog",
+    "nofirepanic",
+    "chester",
+    "mossling",
+    "perd",
+    "catcoon",
+    "deer",
     "shadowwaxwell"
 }
 
@@ -78,7 +204,9 @@ if GetModConfigData("wixie_walter") then
         "extra_claustrophobia_checks", -- extra tag that wixie checks when registering claustrophobia, for stuff like jackolanterns and ruins relics
         "slingshot", -- stuff for new slingshot aiming and wixie exclusivity
         "walter", -- all of walters things, including woby action
-        "wobysmall", "wobybig", "wormhole", -- wixie loses more sanity from wormholes
+        "wobysmall",
+        "wobybig",
+        "wormhole", -- wixie loses more sanity from wormholes
         "slingshotammo", -- removes hunger value from slingshot ammo, preventing slurtle feeding strats
         "coconut" -- shoot a coconut
     }
@@ -90,13 +218,9 @@ if GetModConfigData("wixie_walter") then
 
     }
 
-    for k, v in ipairs(wixie_prefabs) do
-        modimport("wixie_postinit/prefabs/" .. v)
-    end
+    for k, v in ipairs(wixie_prefabs) do modimport("wixie_postinit/prefabs/" .. v) end
 
-    for k, v in ipairs(wixie_components) do
-        modimport("wixie_postinit/components/" .. v)
-    end
+    for k, v in ipairs(wixie_components) do modimport("wixie_postinit/components/" .. v) end
 
     modimport("wixie_postinit/walter_actions")
     modimport("wixie_postinit/widgets/controls") -- Claustrophobia overlay init
@@ -108,30 +232,18 @@ if GetModConfigData("wixie_walter") then
     modimport("wixie_postinit/walter_strings")
     modimport("wixie_postinit/wixie_strings")
 
-    RemapSoundEvent("dontstarve/characters/wixie/death_voice",
-                    "wixie/characters/wixie/death_voice")
-    RemapSoundEvent("dontstarve/characters/wixie/hurt",
-                    "wixie/characters/wixie/hurt")
-    RemapSoundEvent("dontstarve/characters/wixie/talk_LP",
-                    "wixie/characters/wixie/talk_LP")
-    RemapSoundEvent("dontstarve/characters/wixie/ghost_LP",
-                    "wixie/characters/wixie/ghost_LP")
-    RemapSoundEvent("dontstarve/characters/wixie/nightmare_LP",
-                    "wixie/characters/wixie/nightmare_LP")
-    RemapSoundEvent("dontstarve/characters/wixie/yawn",
-                    "wixie/characters/wixie/yawn")
-    RemapSoundEvent("dontstarve/characters/wixie/emote",
-                    "wixie/characters/wixie/emote")
-    RemapSoundEvent("dontstarve/characters/wixie/pose",
-                    "wixie/characters/wixie/pose")
-    RemapSoundEvent("dontstarve/characters/wixie/yawn",
-                    "wixie/characters/wixie/yawn")
-    RemapSoundEvent("dontstarve/characters/wixie/eye_rub_vo",
-                    "wixie/characters/wixie/eye_rub_vo")
-    RemapSoundEvent("dontstarve/characters/wixie/carol",
-                    "wixie/characters/wixie/carol")
-    RemapSoundEvent("dontstarve/characters/wixie/sinking",
-                    "wixie/characters/wixie/sinking")
+    RemapSoundEvent("dontstarve/characters/wixie/death_voice", "wixie/characters/wixie/death_voice")
+    RemapSoundEvent("dontstarve/characters/wixie/hurt", "wixie/characters/wixie/hurt")
+    RemapSoundEvent("dontstarve/characters/wixie/talk_LP", "wixie/characters/wixie/talk_LP")
+    RemapSoundEvent("dontstarve/characters/wixie/ghost_LP", "wixie/characters/wixie/ghost_LP")
+    RemapSoundEvent("dontstarve/characters/wixie/nightmare_LP", "wixie/characters/wixie/nightmare_LP")
+    RemapSoundEvent("dontstarve/characters/wixie/yawn", "wixie/characters/wixie/yawn")
+    RemapSoundEvent("dontstarve/characters/wixie/emote", "wixie/characters/wixie/emote")
+    RemapSoundEvent("dontstarve/characters/wixie/pose", "wixie/characters/wixie/pose")
+    RemapSoundEvent("dontstarve/characters/wixie/yawn", "wixie/characters/wixie/yawn")
+    RemapSoundEvent("dontstarve/characters/wixie/eye_rub_vo", "wixie/characters/wixie/eye_rub_vo")
+    RemapSoundEvent("dontstarve/characters/wixie/carol", "wixie/characters/wixie/carol")
+    RemapSoundEvent("dontstarve/characters/wixie/sinking", "wixie/characters/wixie/sinking")
 end
 
 if GetModConfigData("hangyperds") then
@@ -170,14 +282,10 @@ if GetModConfigData("rework_minotaur") then
     table.insert(prefab_post, "pillar_ruins")
 end
 
-if GetModConfigData("harder_dragonfly") then
-    table.insert(prefab_post, "dragonfly")
-end
+if GetModConfigData("harder_dragonfly") then table.insert(prefab_post, "dragonfly") end
 if GetModConfigData("harder_lavae") then table.insert(prefab_post, "lavae") end
 
-if GetModConfigData("pinelings") then
-    table.insert(prefab_post, "evergreen_stump")
-end
+if GetModConfigData("pinelings") then table.insert(prefab_post, "evergreen_stump") end
 
 if GetModConfigData("canedurability") then table.insert(prefab_post, "cane") end
 
@@ -198,39 +306,27 @@ if GetModConfigData("harder_walrus") then
     table.insert(prefab_post, "walrus")
     table.insert(stategraph_post, "walrus")
 end
-if GetModConfigData("harder_beefalo") then
-    table.insert(stategraph_post, "Beefalo")
-end
+if GetModConfigData("harder_beefalo") then table.insert(stategraph_post, "Beefalo") end
 
 if GetModConfigData("harder_spiderqueen") then
     table.insert(prefab_post, "spiderqueen")
     table.insert(stategraph_post, "spiderqueen")
 end
 
-if GetModConfigData("pocket_powertrip") ~= 0 then
-    table.insert(prefab_post, "pocket_powertrip")
-end
+if GetModConfigData("pocket_powertrip") ~= 0 then table.insert(prefab_post, "pocket_powertrip") end
 
-if GetModConfigData("harder_koalefants") then
-    table.insert(stategraph_post, "koalefant")
-end
+if GetModConfigData("harder_koalefants") then table.insert(stategraph_post, "koalefant") end
 
 if GetModConfigData("reworked_eyes") then
     table.insert(prefab_post, "eyeofterror")
     table.insert(stategraph_post, "eyeofterror")
 end
 
-if GetModConfigData("scalemailbuff") then
-    table.insert(prefab_post, "armor_dragonfly")
-end
+if GetModConfigData("scalemailbuff") then table.insert(prefab_post, "armor_dragonfly") end
 
-if (not GLOBAL:TestForIA() or GetModConfigData("um_music")) then
-    table.insert(component_post, "dynamicmusic")
-end
+if (not GLOBAL:TestForIA() or GetModConfigData("um_music")) then table.insert(component_post, "dynamicmusic") end
 
-if GetModConfigData("winter_burning") and not TUNING.DSTU.ISLAND_ADVENTURES then
-    table.insert(component_post, "burnable")
-end
+if GetModConfigData("winter_burning") and not TUNING.DSTU.ISLAND_ADVENTURES then table.insert(component_post, "burnable") end
 
 if GetModConfigData("amalgams") then table.insert(prefab_post, "chessjunk") end
 --[[
@@ -238,21 +334,13 @@ if GetModConfigData("moon_transformations") then
 	table.insert(prefab_post, "flower")
 end
 ]]
-if GetModConfigData("electricalmishap_") ~= 0 then
-    table.insert(prefab_post, "nightstick")
-end
+if GetModConfigData("electricalmishap_") ~= 0 then table.insert(prefab_post, "nightstick") end
 
-if GetModConfigData("fiendforcedmetodothis") then
-    table.insert(prefab_post, "snurtle")
-end
+if GetModConfigData("fiendforcedmetodothis") then table.insert(prefab_post, "snurtle") end
 
-if GetModConfigData("harder_krampus") then
-    table.insert(stategraph_post, "krampus")
-end
+if GetModConfigData("harder_krampus") then table.insert(stategraph_post, "krampus") end
 
-if GetModConfigData("noauradamage_butterfly") then
-    table.insert(prefab_post, "butterfly")
-end
+if GetModConfigData("noauradamage_butterfly") then table.insert(prefab_post, "butterfly") end
 
 if GetModConfigData("beefalo_nerf") then table.insert(component_post, "rider") end
 
@@ -270,9 +358,7 @@ end
 --	table.insert(prefab_post, "boat")
 -- end
 
-if GetModConfigData("winona_portables_") then
-    table.insert(prefab_post, "winona_portables")
-end
+if GetModConfigData("winona_portables_") then table.insert(prefab_post, "winona_portables") end
 
 if GetModConfigData("reworked_ck") then
     table.insert(prefab_post, "crabking")
@@ -283,15 +369,11 @@ end
 
 if GetModConfigData("hambatnerf") then table.insert(prefab_post, "hambat") end
 
-if GetModConfigData("townportal_rework") then
-    table.insert(prefab_post, "townportal")
-end
+if GetModConfigData("townportal_rework") then table.insert(prefab_post, "townportal") end
 
 if GetModConfigData("monstersmallmeat") then table.insert(prefab_post, "bat") end
 
-if GetModConfigData("cookiecutterhat") then
-    table.insert(prefab_post, "cookiecutterhat")
-end
+if GetModConfigData("cookiecutterhat") then table.insert(prefab_post, "cookiecutterhat") end
 
 modimport("postinit/sim")
 modimport("postinit/any")
