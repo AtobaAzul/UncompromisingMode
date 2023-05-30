ae57c924b0d6c8737f1aa90ed93d70c08cc08157
ShyNuke3D <131507193+ShyNuke@users.noreply.github.com>
2023-05-30 02:27:58 -0300

    Reverted loot from Bunnyman

--- postinit/prefabs/bunnyman.lua
+++ postinit/prefabs/bunnyman.lua
@@ -6,15 +6,5 @@ env.AddPrefabPostInit("bunnyman", function(inst)
 	if not TheWorld.ismastersim then
 		return
 	end
-	
 	inst:AddTag("bunnyattacker")
-	
-	inst:RemoveComponent("lootdropper")
-	
-	inst:AddComponent("lootdropper")
-    inst.components.lootdropper:AddChanceLoot("manrabbit_tail", .25)
-	inst.components.lootdropper:AddRandomLoot("carrot", 1)
-    inst.components.lootdropper:AddRandomLoot("meat", 1)
-    inst.components.lootdropper.numrandomloot = 1
-
 end)
