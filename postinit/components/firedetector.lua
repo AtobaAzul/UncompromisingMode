f6fa56c1c242be7c56936283b160962b3bd1a92e
AtobaAzul <atoba.azul.radical@gmail.com>
2023-10-03 20:10:15 -0300

    Fix crash on start - I am stupid.

--- postinit/components/firedetector.lua
+++ postinit/components/firedetector.lua
@@ -3,7 +3,7 @@ GLOBAL.setfenv(1, GLOBAL)
 
 local UpvalueHacker = require("tools/upvaluehacker")
 
-env.AddComponentPostInit("finiteuses", function(self)
+env.AddComponentPostInit("firedetector", function(self)
     local _NOTAGS  = UpvalueHacker.GetUpvalue(self.Activate, "LookForFiresAndFirestarters", "NOTAGS")
     table.insert(_NOTAGS, "campfire")
 
