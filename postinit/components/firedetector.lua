b6a9a514ffe02e1eec30a05d0c494600f634bd29
AtobaAzul <atoba.azul.radical@gmail.com>
2023-10-03 23:33:23 -0300

    fix night light being extinguished by flingo

--- postinit/components/firedetector.lua
+++ postinit/components/firedetector.lua
@@ -6,5 +6,6 @@ local UpvalueHacker = require("tools/upvaluehacker")
 env.AddComponentPostInit("firedetector", function(self)
     local _NOTAGS  = UpvalueHacker.GetUpvalue(self.Activate, "LookForFiresAndFirestarters", "NOTAGS")
     table.insert(_NOTAGS, "campfire")
+    table.insert(_NOTAGS, "NIGHTMARE_fueled")
 
 end)
