7efe4ca46fae90953496436d6413cc55b7bed340
AtobaAzul <atoba.azul.radical@gmail.com>
2023-10-18 12:44:04 -0300

    remoived the test OCEAN_SLUDGE tile

--- modworldgenmain.lua
+++ modworldgenmain.lua
@@ -15,48 +15,6 @@ local GROUND_OCEAN_COLOR = {
     minimap_color = { 46, 32, 18, 64 }
 }
 
-local WAVETINTS =
-{
-    shallow = { 0.8, 0.9, 1 },
-    rough = { 0.65, 0.84, 0.94 },
-    swell = { 0.65, 0.84, 0.94 },
-    brinepool = { 0.65, 0.92, 0.94 },
-    hazardous = { 0.40, 0.50, 0.62 },
-    waterlog = { 1, 1, 1 },
-}
-
-local OCEAN_SLUDGE_COLOR =
-{
-    primary_color = { 25, 125, 100, 200 },
-    secondary_color = { 25, 125, 100, 255 / 2 },
-    secondary_color_dusk = { 25, 125, 100, 125 },
-    minimap_color = { 0, 0, 0, 255 },
-}
-
-
-AddTile( --RANDOM TEST TILE
-    "OCEAN_SLUDGE",
-    "OCEAN",
-    { ground_name = "Sludge Water" },
-    {
-        name = "cave",
-        noise_texture = "ocean_noise",
-        runsound = "dontstarve/movement/run_marsh",
-        walksound = "dontstarve/movement/walk_marsh",
-        snowsound = "dontstarve/movement/run_ice",
-        mudsound = "dontstarve/movement/run_mud",
-        ocean_depth = "SHALLOW",
-        colors = OCEAN_SLUDGE_COLOR,
-        wavetint = { 249, 180, 45 }
-    },
-    {
-        name = "map_edge",
-        noise_texture = "ocean_noise",
-    }
-)
-
-
-ChangeTileRenderOrder(WORLD_TILES.OCEAN_SLUDGE, WORLD_TILES.OCEAN_BRINEPOOL, false)
 
 AddTile("HOODEDFOREST", -- tile_name 1
     "LAND",             -- tile_range 2
