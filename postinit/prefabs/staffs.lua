988a0a273b56b50744fdd6f076a4867ccd3864f4
AtobaAzul <atoba.azul.radical@gmail.com>
2023-11-02 12:14:33 -0300

    Halved wathom's brightshade staff damage.

--- postinit/prefabs/staffs.lua
+++ postinit/prefabs/staffs.lua
@@ -535,39 +535,37 @@ end
 
 
 
-local function SpikeWaves(inst, target, attacker, angle, fx)
-	if fx ~= nil then
-		local target_index = {}
-		local ix, iy, iz = inst.Transform:GetWorldPosition()
-		local rad = math.rad(angle)
-		local velx = math.cos(rad) * 1.25
-		local velz = -math.sin(rad) * 1.25
-		for i = 1, 5 do
-			inst:DoTaskInTime(FRAMES * i * 1.5, function()
-				local dx, dy, dz = ix + (i * velx), 0, iz + (i * velz)
-				fx.Transform:SetPosition(dx + math.random(), dy, dz + math.random())
-				fx:RestartFX(0.25 + math.random())
-				fx:DoTaskInTime(math.random() + 0.5 , fx.KillFX)
+local function SpikeWaves(inst, target, attacker, angle)
+    local target_index = {}
+    local ix, iy, iz = inst.Transform:GetWorldPosition()
+    local rad = math.rad(angle)
+    local velx = math.cos(rad) * 1.25
+    local velz = -math.sin(rad) * 1.25
+    for i = 1, 5 do
+        inst:DoTaskInTime(FRAMES * i * 1.5, function()
+            local dx, dy, dz = ix + (i * velx), 0, iz + (i * velz)
+            local fx = SpawnPrefab("warg_mutated_ember_fx")
+            fx.Transform:SetPosition(dx + math.random(), dy, dz + math.random())
+            fx:RestartFX(0.25 + math.random())
+            fx:DoTaskInTime(math.random() + 0.5 , fx.KillFX)
 
-				if math.random() > 0.5 then
-					local fx2 = SpawnPrefab("warg_mutated_breath_fx")
-					fx2.Transform:SetPosition(dx + math.random(), dy, dz + math.random())
-					fx2:RestartFX(.25 + math.random())
-					fx2:DoTaskInTime(math.random() + 0.5, fx2.KillFX)
-					fx2.Transform:SetScale(0.5, 0.5, 0.5)
-				end
-				inst:DoTaskInTime(.6, function()
-					local ents = TheSim:FindEntities(dx, dy, dz, 1.5, { "_health", "_combat" }, { "FX", "NOCLICK", "INLIMBO", "notarget" })
-					for k, v in ipairs(ents) do
-						if not target_index[v] and v ~= inst and v.components.combat ~= nil and attacker.components.combat ~= nil and attacker.components.combat:IsValidTarget(v) then
-							target_index[v] = true
-							v.components.combat:GetAttacked(attacker, 0, nil, nil, { planar = 34 })
-						end
-					end
-				end)
-			end)
-		end
-	end
+            if math.random() > 0.5 then
+                local fx2 = SpawnPrefab("warg_mutated_breath_fx")
+                fx2.Transform:SetPosition(dx + math.random(), dy, dz + math.random())
+                fx2:RestartFX(.25 + math.random())
+                fx2:DoTaskInTime(math.random() + 0.5, fx2.KillFX)
+                fx2.Transform:SetScale(0.5, 0.5, 0.5)
+            end
+            inst:DoTaskInTime(.6, function()
+                local ents = TheSim:FindEntities(dx, dy, dz, 1.5, { "_health", "_combat" }, { "FX", "NOCLICK", "INLIMBO", "notarget", "player", "playerghost", "companion"})
+                for k, v in ipairs(ents) do
+                    if  v ~= inst and v.components.combat ~= nil and attacker.components.combat ~= nil and attacker.components.combat:IsValidTarget(v) then
+                        v.components.combat:GetAttacked(attacker, 0, nil, nil, { planar = 17.5 })
+                    end
+                end
+            end)
+        end)
+    end
 end
 
 env.AddPrefabPostInit("staff_lunarplant", function(inst)
@@ -578,19 +576,16 @@ env.AddPrefabPostInit("staff_lunarplant", function(inst)
 
     local function OnAttack(inst, attacker, target, skipsanity)
         if attacker:HasTag("wathom") then
-			local fx = SpawnPrefab("warg_mutated_ember_fx")
-			if fx ~= nil then
-				inst.components.weapon:SetProjectile(nil)
-				local ret = _onattack(inst, attacker, target, skipsanity)
+            inst.components.weapon:SetProjectile(nil)
+            local ret = _onattack(inst, attacker, target, skipsanity)
 
-				for angle = -20, 20, 4 do
-					SpikeWaves(inst, target, attacker, angle + attacker.Transform:GetRotation(), fx)
-					target.components.combat:GetAttacked(attacker, 0, nil, nil, { planar = 34 })
-				end
-				inst.SoundEmitter:PlaySound("rifts/lunarthrall_bomb/explode")
+            for angle = -20, 20, 4 do
+                SpikeWaves(inst, target, attacker, angle + attacker.Transform:GetRotation())
+                target.components.combat:GetAttacked(attacker, 0, nil, nil, { planar = 34 })
+            end
+            inst.SoundEmitter:PlaySound("rifts/lunarthrall_bomb/explode")
 
-				return ret
-			end
+            return ret
         else
             inst.components.weapon:SetProjectile("brilliance_projectile_fx")
             return _onattack(inst, attacker, target, skipsanity)
