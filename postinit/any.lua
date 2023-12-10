babb3269898cbde889f4ed8e79cbdfdcba308577
AtobaAzul <atoba.azul.radical@gmail.com>
2023-12-10 02:11:12 -0300

    players no longer collide on boats.

--- postinit/any.lua
+++ postinit/any.lua
@@ -58,7 +58,90 @@ local IMPASSABLES = {
     ["skeleton"] = true,
     ["skeleton_player"] = true
 }
-local IMPASSABLES_STATUES = {["carrot_oversized"] = true, ["onion_oversized"] = true, ["garlic_oversized"] = true, ["tomato_oversized"] = true, ["potato_oversized"] = true, ["pomegranate_oversized"] = true, ["watermelon_oversized"] = true, ["pumpkin_oversized"] = true, ["pepper_oversized"] = true, ["corn_oversized"] = true, ["durian_oversized"] = true, ["eggplant_oversized"] = true, ["asparagus_oversized"] = true, ["dragonfruit_oversized"] = true, ["carrot_oversized_rotten"] = true, ["onion_oversized_rotten"] = true, ["garlic_oversized_rotten"] = true, ["tomato_oversized_rotten"] = true, ["potato_oversized_rotten"] = true, ["pomegranate_oversized_rotten"] = true, ["watermelon_oversized_rotten"] = true, ["pumpkin_oversized_rotten"] = true, ["pepper_oversized_rotten"] = true, ["corn_oversized_rotten"] = true, ["durian_oversized_rotten"] = true, ["eggplant_oversized_rotten"] = true, ["asparagus_oversized_rotten"] = true, ["dragonfruit_oversized_rotten"] = true, ["carrot_oversized_waxed"] = true, ["onion_oversized_waxed"] = true, ["garlic_oversized_waxed"] = true, ["tomato_oversized_waxed"] = true, ["potato_oversized_waxed"] = true, ["pomegranate_oversized_waxed"] = true, ["watermelon_oversized_waxed"] = true, ["pumpkin_oversized_waxed"] = true, ["pepper_oversized_waxed"] = true, ["corn_oversized_waxed"] = true, ["durian_oversized_waxed"] = true, ["eggplant_oversized_waxed"] = true, ["asparagus_oversized_waxed"] = true, ["dragonfruit_oversized_waxed"] = true, ["chesspiece_hornucopia"] = true, ["chesspiece_pipe"] = true, ["chesspiece_pawn"] = true, ["chesspiece_rook"] = true, ["chesspiece_knight"] = true, ["chesspiece_bishop"] = true, ["chesspiece_muse"] = true, ["chesspiece_formal"] = true, ["chesspiece_deerclops"] = true, ["chesspiece_bearger"] = true, ["chesspiece_moosegoose"] = true, ["chesspiece_dragonfly"] = true, ["chesspiece_clayhound"] = true, ["chesspiece_claywarg"] = true, ["chesspiece_butterfly"] = true, ["chesspiece_anchor"] = true, ["chesspiece_moon"] = true, ["chesspiece_minotaur"] = true, ["chesspiece_toadstool"] = true, ["chesspiece_beequeen"] = true, ["chesspiece_klaus"] = true, ["chesspiece_antlion"] = true, ["chesspiece_stalker"] = true, ["chesspiece_malbatross"] = true, ["chesspiece_crabking"] = true, ["chesspiece_guardianphase3"] = true, ["chesspiece_eyeofterror"] = true, ["chesspiece_twinsofterror"] = true, ["chesspiece_beefalo"] = true, ["chesspiece_carrat"] = true, ["chesspiece_kitcoon"] = true, ["chesspiece_catcoon"] = true, ["sunkenchest"] = true, ["oceantreenut"] = true, ["shell_cluster"] = true, ["cavein_boulder"] = true, ["glassspike_short"] = true, ["glassspike_med"] = true, ["glassspike_tall"] = true, ["potatosack"] = true}
+local IMPASSABLES_STATUES = {
+    ["carrot_oversized"] = true,
+    ["onion_oversized"] = true,
+    ["garlic_oversized"] = true,
+    ["tomato_oversized"] = true,
+    ["potato_oversized"] = true,
+    ["pomegranate_oversized"] = true,
+    ["watermelon_oversized"] = true,
+    ["pumpkin_oversized"] = true,
+    ["pepper_oversized"] = true,
+    ["corn_oversized"] = true,
+    ["durian_oversized"] = true,
+    ["eggplant_oversized"] = true,
+    ["asparagus_oversized"] = true,
+    ["dragonfruit_oversized"] = true,
+    ["carrot_oversized_rotten"] = true,
+    ["onion_oversized_rotten"] = true,
+    ["garlic_oversized_rotten"] = true,
+    ["tomato_oversized_rotten"] = true,
+    ["potato_oversized_rotten"] = true,
+    ["pomegranate_oversized_rotten"] = true,
+    ["watermelon_oversized_rotten"] = true,
+    ["pumpkin_oversized_rotten"] = true,
+    ["pepper_oversized_rotten"] = true,
+    ["corn_oversized_rotten"] = true,
+    ["durian_oversized_rotten"] = true,
+    ["eggplant_oversized_rotten"] = true,
+    ["asparagus_oversized_rotten"] = true,
+    ["dragonfruit_oversized_rotten"] = true,
+    ["carrot_oversized_waxed"] = true,
+    ["onion_oversized_waxed"] = true,
+    ["garlic_oversized_waxed"] = true,
+    ["tomato_oversized_waxed"] = true,
+    ["potato_oversized_waxed"] = true,
+    ["pomegranate_oversized_waxed"] = true,
+    ["watermelon_oversized_waxed"] = true,
+    ["pumpkin_oversized_waxed"] = true,
+    ["pepper_oversized_waxed"] = true,
+    ["corn_oversized_waxed"] = true,
+    ["durian_oversized_waxed"] = true,
+    ["eggplant_oversized_waxed"] = true,
+    ["asparagus_oversized_waxed"] = true,
+    ["dragonfruit_oversized_waxed"] = true,
+    ["chesspiece_hornucopia"] = true,
+    ["chesspiece_pipe"] = true,
+    ["chesspiece_pawn"] = true,
+    ["chesspiece_rook"] = true,
+    ["chesspiece_knight"] = true,
+    ["chesspiece_bishop"] = true,
+    ["chesspiece_muse"] = true,
+    ["chesspiece_formal"] = true,
+    ["chesspiece_deerclops"] = true,
+    ["chesspiece_bearger"] = true,
+    ["chesspiece_moosegoose"] = true,
+    ["chesspiece_dragonfly"] = true,
+    ["chesspiece_clayhound"] = true,
+    ["chesspiece_claywarg"] = true,
+    ["chesspiece_butterfly"] = true,
+    ["chesspiece_anchor"] = true,
+    ["chesspiece_moon"] = true,
+    ["chesspiece_minotaur"] = true,
+    ["chesspiece_toadstool"] = true,
+    ["chesspiece_beequeen"] = true,
+    ["chesspiece_klaus"] = true,
+    ["chesspiece_antlion"] = true,
+    ["chesspiece_stalker"] = true,
+    ["chesspiece_malbatross"] = true,
+    ["chesspiece_crabking"] = true,
+    ["chesspiece_guardianphase3"] = true,
+    ["chesspiece_eyeofterror"] = true,
+    ["chesspiece_twinsofterror"] = true,
+    ["chesspiece_beefalo"] = true,
+    ["chesspiece_carrat"] = true,
+    ["chesspiece_kitcoon"] = true,
+    ["chesspiece_catcoon"] = true,
+    ["sunkenchest"] = true,
+    ["oceantreenut"] = true,
+    ["shell_cluster"] = true,
+    ["cavein_boulder"] = true,
+    ["glassspike_short"] = true,
+    ["glassspike_med"] = true,
+    ["glassspike_tall"] = true,
+    ["potatosack"] = true
+}
 
 if TUNING.DSTU.IMPASSBLES then
     env.AddPrefabPostInitAny(function(inst)
@@ -140,6 +223,8 @@ local function AdjustVisibility(inst, distance, angle)
         inst.um_visibilityclient = -(seebehinddistance - distance) + inst.um_visibilityclient
         if inst.um_visibilityclient > 1 then inst.um_visibilityclient = 1 end
     end
+    print("distance ", distance)
+    print("visclient ", inst.um_visibilityclient)
     if inst.um_visibilityclient <= 0 then
         FullHide(inst)
     else
@@ -178,30 +263,33 @@ local function AdjustVisibility(inst, distance, angle)
 end
 
 local function UpdateVisibility(inst)
-    if ThePlayer and ThePlayer:HasTag("um_darkwood") then
-        local distance = math.sqrt(ThePlayer:GetDistanceSqToInst(inst))
-        local x, y, z = inst.Transform:GetWorldPosition()
-        local x1, y1, z1 = ThePlayer.Transform:GetWorldPosition()
-        local x2, z2
-        if ThePlayer.um_mouseposition_x then
-            x2 = ThePlayer.um_mouseposition_x
-        else
-            x2 = x1 + 1
-        end
-        if ThePlayer.um_mouseposition_z then
-            z2 = ThePlayer.um_mouseposition_z
-        else
-            z2 = z1 + 1
-        end
-        local angle1 = -math.atan2((z2 - z1), (x2 - x1))
-        local angle2 = -math.atan2(z - z1, x - x1)
-        local angle = math.abs(angle1 - angle2)
-        if distance > 20 then
-            FullHide(inst)
-        else
-            AdjustVisibility(inst, distance, angle)
-        end
+    print("hello")
+    --if ThePlayer and ThePlayer:HasTag("um_darkwood") then
+    local distance = math.sqrt(ThePlayer:GetDistanceSqToInst(inst))
+    local x, y, z = inst.Transform:GetWorldPosition()
+    local x1, y1, z1 = ThePlayer.Transform:GetWorldPosition()
+    local x2, z2
+    if ThePlayer.um_mouseposition_x then
+        x2 = ThePlayer.um_mouseposition_x
+    else
+        x2 = x1 + 1
     end
+    if ThePlayer.um_mouseposition_z then
+        z2 = ThePlayer.um_mouseposition_z
+    else
+        z2 = z1 + 1
+    end
+    local angle1 = -math.atan2((z2 - z1), (x2 - x1))
+    local angle2 = -math.atan2(z - z1, x - x1)
+    local angle = math.abs(angle1 - angle2)
+    print("Adjusting")
+    print("Distance")
+    if distance > 20 then
+        FullHide(inst)
+    else
+        AdjustVisibility(inst, distance, angle)
+    end
+    --end
 end
 
 local function PrepareVisibility(inst)
@@ -222,6 +310,37 @@ env.AddPrefabPostInitAny(function(inst)
     if TheWorld.ismastersim then return inst end
     if ThePlayer ~= inst and (inst.entity or inst.replica.inventoryitem) and (inst:HasTag("monster") or inst:HasTag("smallcreature") or inst:HasTag("EPIC") or inst:HasTag("animal") or inst:HasTag("largecreature") or inst:HasTag("character") or inst.prefab == "carrot_planted" or inst.prefab == "red_mushroom" or inst.prefab == "green_mushroom" or inst.prefab == "blue_mushroom" or inst.prefab == "lichen" or inst:HasTag("oceanfishinghookable")) then
         -- if inst.prefab == "shadowheart" then
-        -- PrepareVisibility(inst)
+        --PrepareVisibility(inst)
     end
 end)
+
+local _AddPlatformFollower = EntityScript.AddPlatformFollower
+local _RemovePlatformFollower = EntityScript.RemovePlatformFollower
+
+function EntityScript:AddPlatformFollower(child)
+    if child ~= nil then
+        _AddPlatformFollower(self, child)
+    end
+    if child ~= nil and child:HasTag("player") and not child:HasTag("playerghost") then
+        child.Physics:ClearCollidesWith(COLLISION.CHARACTERS)
+        child.Physics:ClearCollidesWith(COLLISION.OBSTACLES)
+        child.Physics:ClearCollidesWith(COLLISION.SMALLOBSTACLES)
+        child.Physics:ClearCollidesWith(COLLISION.GIANTS)
+    end
+end
+
+function EntityScript:RemovePlatformFollower(child)
+    if child ~= nil then
+        _RemovePlatformFollower(self, child)
+    end
+
+
+    if child ~= nil and child:HasTag("player") and not child:HasTag("playerghost") and not child.sg.mem.isobstaclepassthrough and not child.sg.mem.ischaracterpassthrough then
+        child:DoTaskInTime(1, function(inst)
+            inst.Physics:CollidesWith(COLLISION.OBSTACLES)
+            inst.Physics:CollidesWith(COLLISION.SMALLOBSTACLES)
+            inst.Physics:CollidesWith(COLLISION.CHARACTERS)
+            inst.Physics:CollidesWith(COLLISION.GIANTS)
+        end)
+    end
+end
