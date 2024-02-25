d1390e522a41dcd401161217678cf3de47757b6a
AtobaAzul <atoba.azul.radical@gmail.com>
2024-02-25 13:39:45 -0300

    brought back lighter sucking up smog

--- init/init_character_changes/willow.lua
+++ init/init_character_changes/willow.lua
@@ -1,2 +1,36 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
+
+env.AddPrefabPostInit("lighter", function(inst)
+    if not TheWorld.ismastersim then return end
+
+    local _testforattunedskill = inst.testforattunedskill
+
+    inst.testforattunedskill = function(inst, owner)
+        _testforattunedskill(inst, owner)
+        if owner.components.skilltreeupdater:IsActivated("willow_attuned_lighter") and inst.components.channelcastable ~= nil then
+            local _onstartchannelingfn = inst.components.channelcastable.onstartchannelingfn
+            local _onstopchannelingfn = inst.components.channelcastable.onstopchannelingfn
+
+            inst.components.channelcastable.onstopchannelingfn  = function(inst, user)
+                _onstopchannelingfn(inst, user)
+                if inst.smog_task then
+                    inst.smog_task:Cancel()
+                    inst.smog_task = nil
+                end
+            end
+
+            inst.components.channelcastable.onstartchannelingfn = function(inst, user)
+                _onstartchannelingfn(inst, user)
+                inst.smog_task = inst:DoPeriodicTask(0.3, function(inst)
+                    local x, y, z = inst.Transform:GetWorldPosition()
+                    local smog = TheSim:FindEntities(x, y, z, 12, { "smog" })
+                    for k, v in pairs(smog) do
+                        v:Remove()
+                        inst.SoundEmitter:PlaySound("meta3/willow_lighter/ember_absorb")
+                    end
+                end, nil, user)
+            end
+        end
+    end
+end)
