c4b6673960fffbe6fbe5dd531f050ca22f13b1e6
KoreanWaffles <koreanwafflesds@gmail.com>
2024-03-12 07:55:21 +0000

    Update 2 files
    - /postinit/components/foodaffinity.lua
    - /postinit/components/edible.lua

--- postinit/components/edible.lua
+++ postinit/components/edible.lua
@@ -1,9 +1,17 @@
+--[[
+Documentation: KoreanWaffles
+
+Please refer to postinit/components/foodaffinity for related changes
+and further documentation regarding characters' favorite foods.
+]]
+
 if not GLOBAL.TheNet:GetIsServer() then return end
 
 AddComponentPostInit("edible", function(self)
     local _GetHunger = self.GetHunger
     local _GetSanity = self.GetSanity
 
+    -- Reverse the bonus hunger granted by favorite foods.
     self.GetHunger = function(self, eater)
         local hungervalue = _GetHunger(self, eater)
         local multiplier = 1
@@ -43,22 +51,30 @@ AddComponentPostInit("edible", function(self)
         end
     end
 
+    -- Calculations for favorite food sanity.
     local _GetSanity = self.GetSanity
-
     self.GetSanity = function(self, eater)
         local sanityvalue = _GetSanity(self, eater) or 0
         local addend = 0
-
+        
         if eater and eater.components.foodaffinity then
             local affinity_bonus = eater.components.foodaffinity.prefab_affinites[self.inst.prefab]
-            if affinity_bonus then
+            local favorite_foods = eater.components.foodaffinity.favorite_foods
+            local favorite_food_fn = eater.components.foodaffinity.favorite_food_fn
+            -- check if this food is already defined as the character's favorite food
+            if favorite_foods and favorite_foods[self.inst.prefab] then
+                addend = favorite_foods[self.inst.prefab]
+            -- check for the affinity bonus or if this food satisfies the eater's favorite food function
+            elseif affinity_bonus or favorite_food_fn and favorite_food_fn(self.inst) then
+                -- favorite foods will not incur sanity penalities
                 if sanityvalue < 0 then
                     sanityvalue = 0
                 end
                 local hungervalue = self.GetHunger(self, eater)
                 local healthvalue = self.GetHealth(self, eater)
-                local favoritefood_sanity = eater.components.foodaffinity.favoritefood_sanity
-                addend = favoritefood_sanity or math.max(5, healthvalue / 4, hungervalue / 5, sanityvalue / 3)
+                -- Since no sanity value was specified in favorite_foods, bonus sanity will be
+                -- calculated dynamically based on the stats of the food, with a minumum value of 5.
+                addend = math.max(5, healthvalue / 4, hungervalue / 5, sanityvalue / 3)
             end
         end
 
