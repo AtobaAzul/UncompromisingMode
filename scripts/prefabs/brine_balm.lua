28b2ffe43c29fd1445d8477f2900a491e0a1767f
AtobaAzul <atoba.azul.radical@gmail.com>
2024-03-17 19:46:14 -0300

    **BRINE BALM** heals max hp HAHAHAHAH >:)

--- scripts/prefabs/brine_balm.lua
+++ scripts/prefabs/brine_balm.lua
@@ -4,10 +4,11 @@ local assets =
 }
 
 local function OnUse(inst, target)
-	if target.components.debuffable ~= nil and target.components.health ~= nil and not target.components.health:IsDead() then
-		target.configheal = 70
-		target.components.debuffable:AddDebuff("confighealbuff", "confighealbuff")
-	end
+    if target.components.debuffable ~= nil and target.components.health ~= nil and not target.components.health:IsDead() then
+        target.components.health:DeltaPenalty(-.125)
+        target.configheal = 70
+        target.components.debuffable:AddDebuff("confighealbuff", "confighealbuff")
+    end
 end
 
 local function fn()
