40b6ba283bdf16bd7a29053cbd5f03683e2b25d7
AtobaAzul <atoba.azul.radical@gmail.com>
2024-03-18 00:43:16 -0300

    boats following magnets take less damage.

--- postinit/components/hullhealth.lua
+++ postinit/components/hullhealth.lua
@@ -2,13 +2,28 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 
 env.AddComponentPostInit("hullhealth", function(self)
+    local _GetDamageMult = self.GetDamageMult
+
+    function self:GetDamageMult(cat, ...)
+        local ret = _GetDamageMult(self, ...)
+
+        if self.inst.components.boatphysics ~= nil then
+            for k, v in pairs(self.inst.components.boatphysics.magnets) do
+                print("magnet found! reducing damage.")
+                ret = ret * 0.25
+                break
+            end
+        end
+
+        return ret
+    end
+
     local _OnCollide = self.OnCollide
 
-    function self:OnCollide(data)
-        if data.other ~= nil and data.other:HasTag("boat") and not TheNet:GetPVPEnabled() then
+    function self:OnCollide(data, ...)
+        if data.other ~= nil and (data.other:HasTag("boat") or data.other.components.boatphysics ~= nil) and not TheNet:GetPVPEnabled() then
             return
-        else
-            return _OnCollide(self, data)
         end
+        return _OnCollide(self, data)
     end
 end)
