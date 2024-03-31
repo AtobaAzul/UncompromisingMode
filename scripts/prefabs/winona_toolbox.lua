aaa7414a6c2ce320ed913ff750ab689ea12e39fc
AtobaAzul <atoba.azul.radical@gmail.com>
2024-03-31 12:45:51 -0300

    toolbox can be used in the inventory... i think

--- scripts/prefabs/winona_toolbox.lua
+++ scripts/prefabs/winona_toolbox.lua
@@ -1,22 +1,22 @@
 require "prefabutil"
 
 local function onopen(inst)
-	if not inst:HasTag("burnt") then
-		inst.AnimState:PlayAnimation("open", false)
-		inst.SoundEmitter:PlaySound("hookline/common/fishbox/open")
-	end
+    if not inst:HasTag("burnt") then
+        inst.AnimState:PlayAnimation("open", false)
+        inst.SoundEmitter:PlaySound("hookline/common/fishbox/open")
+    end
 end
 
 local function onclose(inst)
     if not inst:HasTag("burnt") then
         inst.AnimState:PlayAnimation("close", false)
-    inst.SoundEmitter:PlaySound("saltydog/common/saltbox/close")
+        inst.SoundEmitter:PlaySound("saltydog/common/saltbox/close")
     end
 end
 
 local function OnPutInInventory(inst)
-	inst.components.container:Close()
-	inst.AnimState:PlayAnimation("idle", false)
+    inst.components.container:Close()
+    inst.AnimState:PlayAnimation("idle", false)
 end
 
 local function onsave(inst, data)
@@ -33,7 +33,7 @@ end
 
 local function MakeTackleContainer(name, bank, build, assets)
     assets = assets or {}
-    table.insert(assets, Asset("ANIM", "anim/"..build..".zip"))
+    table.insert(assets, Asset("ANIM", "anim/" .. build .. ".zip"))
 
     local function fn()
         local inst = CreateEntity()
@@ -44,18 +44,19 @@ local function MakeTackleContainer(name, bank, build, assets)
         inst.entity:AddMiniMapEntity()
         inst.entity:AddNetwork()
 
-		inst.MiniMapEntity:SetIcon(name..".png")
-		
-		inst:AddTag("waterproofer")
+        inst.MiniMapEntity:SetIcon(name .. ".png")
+
+        inst:AddTag("waterproofer")
+        inst:AddTag("portablestorage")
 
         inst.AnimState:SetBank(bank)
         inst.AnimState:SetBuild(build)
         inst.AnimState:PlayAnimation("idle")
-		inst.Transform:SetScale(2, 2, 2)
+        inst.Transform:SetScale(2, 2, 2)
 
-		MakeInventoryPhysics(inst)
+        MakeInventoryPhysics(inst)
 
-        local swap_data = {bank = bank, anim = "idle"}
+        local swap_data = { bank = bank, anim = "idle" }
         MakeInventoryFloatable(inst, "small", nil, nil, nil, nil, swap_data)
 
         inst.entity:SetPristine()
@@ -64,24 +65,24 @@ local function MakeTackleContainer(name, bank, build, assets)
             return inst
         end
 
-		inst:AddComponent("inspectable")
+        inst:AddComponent("inspectable")
 
-		inst:AddComponent("waterproofer")
-		inst.components.waterproofer:SetEffectiveness(0)
+        inst:AddComponent("waterproofer")
+        inst.components.waterproofer:SetEffectiveness(0)
 
         inst:AddComponent("container")
         inst.components.container:WidgetSetup("winona_toolbox")
         inst.components.container.onopenfn = onopen
-		inst.components.container.onclosefn = onclose
+        inst.components.container.onclosefn = onclose
         inst.components.container.skipclosesnd = true
         inst.components.container.skipopensnd = true
-		inst.components.container.droponopen = true
+        inst.components.container.droponopen = true
 
-		inst:AddComponent("inventoryitem")
-		inst.components.inventoryitem:SetOnPutInInventoryFn(OnPutInInventory)
-		inst.components.inventoryitem.atlasname = "images/inventoryimages/winona_toolbox.xml"
+        inst:AddComponent("inventoryitem")
+        inst.components.inventoryitem:SetOnPutInInventoryFn(OnPutInInventory)
+        inst.components.inventoryitem.atlasname = "images/inventoryimages/winona_toolbox.xml"
 
-		inst:AddComponent("lootdropper")
+        inst:AddComponent("lootdropper")
 
         inst:AddComponent("hauntable")
         inst.components.hauntable:SetHauntValue(TUNING.HAUNT_TINY)
