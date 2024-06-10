5166172e2f719d1336930c0f187cb4bd729c01e4
AtobaAzul <atoba.azul.radical@gmail.com>
2024-06-10 15:07:32 -0300

    some formatting +stafflight melts snowpiles.

--- postinit/prefabs/rneghostfire.lua
+++ postinit/prefabs/rneghostfire.lua
@@ -19,90 +19,90 @@ env.AddPrefabPostInit("lantern", function(inst)
 inst:AddTag("lighttarget1")
 inst:AddTag("lighttarget")
 --return inst
-end)]]--
+end)]] --
 env.AddPrefabPostInit("stafflight", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget2")
-inst:AddTag("lighttarget")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget2")
+    inst:AddTag("lighttarget")
+    --return inst
 end)
 env.AddPrefabPostInit("staffcoldlight", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget2")
-inst:AddTag("lighttarget")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget2")
+    inst:AddTag("lighttarget")
+    --return inst
 end)
 env.AddPrefabPostInit("campfire", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    --return inst
 end)
 env.AddPrefabPostInit("firepit", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    --return inst
 end)
 env.AddPrefabPostInit("coldfire", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    --return inst
 end)
 env.AddPrefabPostInit("coldfirepit", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    --return inst
 end)
 env.AddPrefabPostInit("pumpkin_lantern", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
-inst:AddTag("plight")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    inst:AddTag("plight")
+    --return inst
 end)
 env.AddPrefabPostInit("mushroom_light", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
-inst:AddTag("mlight1")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    inst:AddTag("mlight1")
+    --return inst
 end)
 env.AddPrefabPostInit("mushroom_light2", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
-inst:AddTag("mlight2")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    inst:AddTag("mlight2")
+    --return inst
 end)
 env.AddPrefabPostInit("dragonflyfurnace", function(inst)
-	if not TheWorld.ismastersim then
-		return
-	end
-	
-inst:AddTag("lighttarget")
-inst:AddTag("dlight")
---return inst
+    if not TheWorld.ismastersim then
+        return
+    end
+
+    inst:AddTag("lighttarget")
+    inst:AddTag("dlight")
+    --return inst
 end)
