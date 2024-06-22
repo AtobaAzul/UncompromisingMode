bf5f9397d380476a1ee51b65be2861302719277f
ShyNuke <131507193+ShyNuke@users.noreply.github.com>
2024-06-22 14:13:40 -0300

    Revert "Optimizations."
    This reverts commit 6e6d44dc3aff69371898c2eaa0a1ebe7192de63d.

--- scripts/prefabs/mara_boss1.lua
+++ scripts/prefabs/mara_boss1.lua
@@ -7,35 +7,21 @@ local prefabs = {
 
 -- Movement here.
 local function SansDans(inst)
-	local nextvictim = FindClosestEntity(inst, 85, true, { "_health" }, { "sans", "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
+	local nextvictim = FindClosestEntity(inst, 85, true, nil, { "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
 	
-	local function Moveit(inst)
-		if nextvictim ~= nil and nextvictim.components.health ~= nil and math.random() > 0.35 then
-			--print ("SSSAAAAAN: I am rapidly approaching your location.")
-			inst.components.locomotor:GoToPoint(Vector3(nextvictim.Transform:GetWorldPosition()), nil, true)
-			inst.components.locomotor:RunForward()
-			inst:DoTaskInTime(0.05, SansDans)
-		-- More potential movement patterns:
-		-- Find target location, double the distance, and apply a sine pattern to path there (randomize sine severity).
-		-- Quarter-circular motion that maintains current distance from the target.
-		else
-			--print ("SSSAAAAAN: Obama OUT")
-			inst.components.locomotor:RunInDirection(math.random() * 359)
-			inst.components.locomotor:RunForward()
-		end
-	end
-	
-	
-	-- Cheap time tracking.
-	if inst.taskcountmove ~= nil then
-		inst.taskcountmove = (inst.taskcountmove + 1)
+	if nextvictim ~= nil and nextvictim.components.health ~= nil and math.random() > 0.35 then
+		--print ("SSSAAAAAN: I am rapidly approaching your location.")
+		inst.components.locomotor:GoToPoint(Vector3(nextvictim.Transform:GetWorldPosition()), nil, true)
+		inst.components.locomotor:RunForward()
+		inst:DoTaskInTime(0.05, SansDans)
+	-- More potential movement patterns:
+	-- Find target location, double the distance, and apply a sine pattern to path there (randomize sine severity).
+	-- Quarter-circular motion that maintains current distance from the target.
 	else
-		inst.taskcountmove = 1
-	end
-	
-	if math.random() > 0.5 or inst.taskcountmove > 1 then
-		Moveit(inst)
-		inst.taskcountmove = 0
+		--print ("SSSAAAAAN: Obama OUT")
+		inst.components.locomotor:RunInDirection(math.random() * 359)
+		inst.components.locomotor:RunForward()
+		inst:DoTaskInTime((math.random(1, 2) * 0.1) + 0.2, SansDans)
 	end
 end
 
@@ -55,68 +41,41 @@ local function BoneZone(inst)
 			-- make the blaster a crappy moonseer phase 1 head
 		--end
 	end
-	
-	-- Cheap time tracking.
-	if inst.taskcountprojectile ~= nil then
-		inst.taskcountprojectile = (inst.taskcountprojectile + 1)
-	else
-		inst.taskcountprojectile = 1
-	end
 
-	if (inst.taskcountprojectile > 2 and math.random() > 0.5) or inst.taskcountprojectile > 4 then
-		local x, y, z = inst.Transform:GetWorldPosition()
-		-- Next line determines how many bones are spawned.
-		local anythingwithafuckinghealthbar = TheSim:FindEntities(x, y, z, 50, { "_health" }, { "sans", "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
-		
-		if #anythingwithafuckinghealthbar ~= nil then
-			for i = 1, (#anythingwithafuckinghealthbar * math.random(2, 5)) do
-				inst:DoTaskInTime(0, Projectile)
-			end
+	local x, y, z = inst.Transform:GetWorldPosition()
+	-- NEXT LINE DETERMINES HOW MANY BONES ARE SPAWNED
+	local anythingwithafuckinghealthbar = TheSim:FindEntities(x, y, z, 50, nil, { "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
+	
+	if #anythingwithafuckinghealthbar ~= nil then
+		for i = 1, #anythingwithafuckinghealthbar do
+			inst:DoTaskInTime(0.2, Projectile)
 		end
-		inst.taskcountprojectile = 0
 	end
+	
+	inst:DoTaskInTime(math.random() * 0.6 + 1, BoneZone)
 end
 
 local function SoundsThatMightPlayWhenYouFightSans(inst)
-	local function CycleSound(inst)
-		inst.SoundEmitter:KillSound("mara_boss1_stmpwyfs")
-		inst.SoundEmitter:PlaySound("stmpwyfs/stmpwyfs/stmpwyfs", "mara_boss1_stmpwyfs")
-		inst.SoundEmitter:SetVolume("mara_boss1_stmpwyfs", 0.85)
-		
-		-- Kill music. TODO: Add as a cycling task to the Moonseer controller, to clear the air for progressive music.
-		TheFocalPoint.SoundEmitter:KillSound("busy")
-		TheFocalPoint.SoundEmitter:KillSound("danger")
-	end
-	
-	-- Cheap time tracking.
-	if inst.taskcountsound ~= nil then
-		inst.taskcountsound = (inst.taskcountsound + 1)
-	else
-		inst.taskcountsound = 1
-	end
-	
-	if (inst.taskcountsound > 1 and math.random() > 0.5) or inst.taskcountsound > 4 then
-		CycleSound(inst)
-		inst.taskcountsound = 0
-	end
+	inst.SoundEmitter:KillSound("mara_boss1_stmpwyfs")
+	inst.SoundEmitter:PlaySound("stmpwyfs/stmpwyfs/stmpwyfs", "mara_boss1_stmpwyfs")
+	inst.SoundEmitter:SetVolume("mara_boss1_stmpwyfs", 0.85)
+	-- If I can make this sound prevent music from playing in a friendly way, I'll have one potential basis for progressive boss music...
+
+	inst:DoTaskInTime(math.random() * 0.6 + 0.3, SoundsThatMightPlayWhenYouFightSans)
 end
 
-
-local function OnEntityWake(inst)
-	if inst.SansPeriodicTask == nil then
-		inst.SansPeriodicTask = inst:DoPeriodicTask(0.2, function()
-			inst:DoTaskInTime(0, SansDans)
-			inst:DoTaskInTime(0, BoneZone)
-			inst:DoTaskInTime(0, SoundsThatMightPlayWhenYouFightSans)
-		end, 3) 
-	end
+local function AwakeFromThySlumber(inst)
+	inst.AnimState:PlayAnimation("loop", true)
+	inst:DoTaskInTime(0, SansDans)
+	inst:DoTaskInTime(0, BoneZone)
+	inst:DoTaskInTime(0, SoundsThatMightPlayWhenYouFightSans)
 end
 
-local function OnEntitySleep(inst)
-	if inst.SansPeriodicTask ~= nil then
-		inst.SansPeriodicTask:Cancel()
-		inst.SansPeriodicTask = nil
-	end
+-- When spawned/loaded, have him stand there...menacingly.
+local function Arise(inst)
+	inst.AnimState:PlayAnimation("idle")
+	-- Pass a few seconds, enough time to take in the mistake. Then...begin.
+	inst:DoTaskInTime(3, AwakeFromThySlumber)
 end
 
 
@@ -140,7 +99,6 @@ local function fn()
 	inst.Transform:SetScale(1.5, 1.5, 1.5)
 	
 	inst:AddTag("scarytoprey")
-	inst:AddTag("sans")
 	
 	inst.Light:Enable(true)
 	inst.Light:SetRadius(4)
@@ -156,8 +114,8 @@ local function fn()
 	end
 	
 	inst:AddComponent("locomotor")
-	inst.components.locomotor.walkspeed = 40
-	inst.components.locomotor.runspeed = 40
+	inst.components.locomotor.walkspeed = ((30 * math.random()) + 15)
+	inst.components.locomotor.runspeed = ((30 * math.random()) + 15)
 	inst.components.locomotor:EnableGroundSpeedMultiplier(false)
 	inst.components.locomotor.pathcaps = { ignorecreep = true }
 	
@@ -165,19 +123,9 @@ local function fn()
 	
 	inst:AddComponent("health")
 	inst.components.health:SetMaxHealth(1)
-	inst.components.health:SetMinHealth(0.0001)
-	inst.components.health:SetInvincible(true) -- REMOVE WHEN THE DROPPED ITEM IS ADDED
+	inst.components.health:SetInvincible(true)
 	
-	inst:AddComponent("combat")
-	
-	
-	-- When spawned/loaded, pass a few seconds, standing there...menacingly. Enough time to take in the mistake. Then...begin.
-	inst:DoTaskInTime(3, function()
-		inst.AnimState:PlayAnimation("loop", true)
-	end)
-	
-	inst.OnEntityWake = OnEntityWake -- Should begin when spawned.
-	inst.OnEntitySleep = OnEntitySleep
+	inst:DoTaskInTime(0, Arise)
 	
 	return inst
 end
--- scripts/prefabs/mara_boss1_bullets.lua
+++ scripts/prefabs/mara_boss1_bullets.lua
@@ -42,41 +42,50 @@ local function OnHitOther(inst, other)
 	end
 end
 
-
--- Here's the part where we violently murder anything we so much as lightly graze :D
--- And anything near it!
-local function OmaeWaMou(inst)
-	local nextvictim = FindClosestEntity(inst, 3, true, { "_health" }, { "sans", "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
-	
-	if nextvictim ~= nil and nextvictim.components.health ~= nil then
-		local AREAATTACK_EXCLUDETAGS = { "sans", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" }
-		
-		inst.components.combat:DoAreaAttack(inst, 3, nil, nil, nil, AREAATTACK_EXCLUDETAGS)
-		
-		local bonebreakfx = SpawnPrefab("rock_break_fx")
-		if bonebreakfx ~= nil then
-			bonebreakfx.Transform:SetPosition(inst.Transform:GetWorldPosition())
-		end
-		inst.SoundEmitter:PlaySound("dontstarve/common/destroy_stone")
-		
-		inst:DoTaskInTime(FRAMES*1, function() inst:Remove() end)
-	end
+local function Perish(inst)
+	inst:Remove()
 end
-
 
 -- Movement here.
 local function WOOSH(inst)
-	local nextvictim = FindClosestEntity(inst, 50, true, { "_health" }, { "sans", "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
+	local nextvictim = FindClosestEntity(inst, 50, true, nil, { "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
 	
 	if nextvictim ~= nil and nextvictim.components.health ~= nil and math.random() > 0.8 then
 		inst:FacePoint(nextvictim.Transform:GetWorldPosition())
 		inst.components.locomotor:RunForward()
---	elseif math.random() > 0.5 then
-	--	Spiral movement pattern.
+	--	Maybe mix in a spiral pattern occasionally? Hm.
 	else
 		inst.components.locomotor:RunInDirection(math.random() * 359)
 		inst.components.locomotor:RunForward()
 	end
+	
+--	inst:DoTaskInTime(3, Perish)
+end
+
+-- Here's the part where we violently murder anything we so much as lightly graze :D
+-- And anything near it!
+local function YEET(inst)
+	local AREAATTACK_EXCLUDETAGS = { "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" }
+	
+	inst.components.combat:DoAreaAttack(inst, 3, nil, nil, nil, AREAATTACK_EXCLUDETAGS)
+	
+	local bonebreakfx = SpawnPrefab("rock_break_fx")
+	if bonebreakfx ~= nil then
+		bonebreakfx.Transform:SetPosition(inst.Transform:GetWorldPosition())
+	end
+	inst.SoundEmitter:PlaySound("dontstarve/common/destroy_stone")
+	
+	inst:DoTaskInTime(0, Perish)
+end
+
+-- Proximity detonator.
+local function OmaeWaMou(inst)
+	local nextvictim = FindClosestEntity(inst, 50, true, nil, { "plant", "blocker", "FX", "INLIMBO", "invisible", "notarget", "noattack", "playerghost" })
+	
+	if nextvictim ~= nil and nextvictim.components.health ~= nil then
+		YEET(inst)
+	end
+	inst:DoTaskInTime(0.1, OmaeWaMou)
 end
 
 
@@ -102,7 +111,6 @@ local function fn()
 	inst.Transform:SetScale((1 + math.random()), (1 + math.random()), (1 + math.random()))
 	
 	inst:AddTag("scarytoprey")
-	inst:AddTag("sans")
 	
 	inst.Light:Enable(true)
 	inst.Light:SetRadius(1)
@@ -118,8 +126,8 @@ local function fn()
 	end
 	
 	inst:AddComponent("locomotor")
-	inst.components.locomotor.walkspeed = ((30 * math.random()) + 35)
-	inst.components.locomotor.runspeed = ((30 * math.random()) + 35)
+	inst.components.locomotor.walkspeed = ((30 * math.random()) + 25)
+	inst.components.locomotor.runspeed = ((30 * math.random()) + 25)
 	inst.components.locomotor:EnableGroundSpeedMultiplier(false)
 	inst.components.locomotor.pathcaps = { ignorecreep = true }
 	
@@ -128,15 +136,9 @@ local function fn()
 	inst.components.combat.onhitotherfn = OnHitOther
 	
 	inst:DoTaskInTime(0, WOOSH)
-	inst:DoPeriodicTask((FRAMES * 4), OmaeWaMou, 0)
+	inst:DoTaskInTime(0, OmaeWaMou)
 	
 	
-	inst:DoTaskInTime(0.8, function()
-		ErodeAway(inst, 10*FRAMES)
-	end)
-	
-	
-	inst.OnEntitySleep = inst.Remove
 	inst.persists = false
 	
 	return inst
