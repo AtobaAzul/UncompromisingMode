fdd39b30d165e5a53424d366aef39035cff14d8e
AtobaAzul <atoba.azul.radical@gmail.com>
2024-07-06 14:00:30 -0300

    winter fire fixes, i think

--- postinit/components/burnable.lua
+++ postinit/components/burnable.lua
@@ -2,48 +2,50 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 ------------------------Fire spread is less efficient in winter-----------------------------------------
 env.AddComponentPostInit("burnable", function(self)
-	local _OldExtendBurning = self.ExtendBurning
-	local _OldStartWildfire = self.StartWildfire
+    local _OldExtendBurning = self.ExtendBurning
+    local _OldStartWildfire = self.StartWildfire
 
-	local function DoneBurning(inst, self)
-		local isplant = inst:HasTag("plant") and
-			not (inst.components.diseaseable ~= nil and inst.components.diseaseable:IsDiseased())
-		local pos = isplant and inst:GetPosition() or nil
+    local function DoneBurning(inst, self)
+        local isplant = inst:HasTag("plant") and not (inst.components.diseaseable ~= nil and inst.components.diseaseable:IsDiseased())
+        local pos = isplant and inst:GetPosition() or nil
 
-		inst:PushEvent("onburnt")
+        inst:PushEvent("onburnt")
 
-		if self.onburnt ~= nil then
-			self.onburnt(inst)
-		end
+        if self.onburnt ~= nil then
+            self.onburnt(inst)
+        end
 
-		if inst.components.explosive ~= nil then
-			inst.components.explosive:OnBurnt()
-		end
+        if self.inst:IsValid() then
+            if inst.components.explosive ~= nil then
+                --explosive explode
+                inst.components.explosive:OnBurnt()
+            end
 
-		if self.extinguishimmediately then
-			self:Extinguish()
-		end
+            if self.extinguishimmediately then
+                self:Extinguish()
+            end
+        end
 
-		if isplant then
-			TheWorld:PushEvent("plantkilled", { pos = pos })
-		end
-	end
+        if isplant then
+            TheWorld:PushEvent("plantkilled", { pos = pos }) --this event is pushed in other places too
+        end
+    end
 
-	function self:ExtendBurning()
-		if TheWorld.state.season == "winter" then
-			if self.task ~= nil then
-				self.task:Cancel()
-			end
-			self.task = self.burntime ~= nil and self.inst:DoTaskInTime(self.burntime * 0.24, DoneBurning, self) or nil
-		else
-			return _OldExtendBurning(self)
-		end
-	end
+    function self:ExtendBurning()
+        if TheWorld.state.season == "winter" then
+            if self.task ~= nil then
+                self.task:Cancel()
+            end
+            self.task = self.burntime ~= nil and self.inst:DoTaskInTime(self.burntime * (self.controlled_burn and self:CalculateControlledBurnDuration() or 1) * 0.24, DoneBurning, self) or nil
+        else
+            return _OldExtendBurning(self)
+        end
+    end
 
-	function self:StartWildfire()
-		local x, y, z = self.inst.Transform:GetWorldPosition()
-		if #TheSim:FindEntities(x, y, z, 12, { "canopy" }) <= 0 or TheWorld:HasTag("heatwavestart") then
-			return _OldStartWildfire(self)
-		end
-	end
+    function self:StartWildfire()
+        local x, y, z = self.inst.Transform:GetWorldPosition()
+        if #TheSim:FindEntities(x, y, z, 12, { "canopy" }) <= 0 or TheWorld:HasTag("heatwavestart") then
+            return _OldStartWildfire(self)
+        end
+    end
 end)
--- postinit/components/propagator.lua
+++ postinit/components/propagator.lua
@@ -1,78 +1,92 @@
 local env = env
 GLOBAL.setfenv(1, GLOBAL)
 ------------------------Fire spread is less efficient in winter-----------------------------------------
+local TARGET_CANT_TAGS = { "INLIMBO" }
+local TARGET_MELT_MUST_TAGS = { "frozen", "firemelt" }
+
 env.AddComponentPostInit("propagator", function(self)
-	local _OldOnUpdate = self.OnUpdate
-	
-	function self:OnUpdate(dt)
-		if TheWorld.state.season == "winter" and not self.inst.sg then
-			self.currentheat = math.max(0, self.currentheat - dt * self.decayrate )
+    local _OldOnUpdate = self.OnUpdate
 
-			local x, y, z = self.inst.Transform:GetWorldPosition()
-			
-			if self.spreading then
-				local pos = self.inst:GetPosition()
-				local prop_range = (self.propagaterange or 3) * TUNING.DSTU.WINTER_FIRE_MOD or 0
-				local ents = TheSim:FindEntities(x, y, z, prop_range, nil, { "INLIMBO" })
-				if ents ~= nil and #ents > 0 then
-					local dmg_range = self.damagerange * TUNING.DSTU.WINTER_FIRE_MOD
-					local dmg_range_sq = dmg_range * dmg_range
-					local prop_range_sq = prop_range * prop_range
-					local isendothermic = self.inst.components.heater ~= nil and self.inst.components.heater:IsEndothermic()
+    function self:OnUpdate(dt)
+        if TheWorld.state.season == "winter" and not self.inst.sg then
+            self:CalculateHeatCap()
 
-					for i, v in ipairs(ents) do
-						if v:IsValid() then
-							local dsq = distsq(pos, v:GetPosition())
+            if self.currentheat > 0 then
+                self.currentheat = math.max(0, self.currentheat - dt * self.decayrate)
+            elseif self.currentheat < 0 then
+                self.currentheat = math.min(0, self.currentheat + dt * self.decayrate)
+            end
 
-							if v ~= self.inst then
-								if v.components.propagator ~= nil and
-									v.components.propagator.acceptsheat and
-									not v.components.propagator.pauseheating then
-									local percent_heat = math.max(.1, 1 - dsq / prop_range_sq)
-									v.components.propagator:AddHeat(self.heatoutput * percent_heat * dt)
-								end
+            local x, y, z = self.inst.Transform:GetWorldPosition()
+            local prop_range = TheWorld.state.iswinter and self.propagaterange * TUNING.DSTU.WINTER_FIRE_MOD or self.propagaterange
 
-								if v.components.freezable ~= nil then
-									v.components.freezable:AddColdness(-.25 * self.heatoutput * dt * TUNING.DSTU.WINTER_FIRE_MOD)
-									if v.components.freezable:IsFrozen() and v.components.freezable.coldness <= 0 then
-										v.components.freezable:Unfreeze()
-									end
-								end
+            if self.spreading then
+                local ents = TheSim:FindEntities(x, y, z, prop_range, nil, TARGET_CANT_TAGS)
+                if #ents > 0 and prop_range > 0 then
+                    local dmg_range = TheWorld.state.iswinter and self.damagerange * TUNING.DSTU.WINTER_FIRE_MOD or self.damagerange
+                    local dmg_range_sq = dmg_range * dmg_range
+                    local prop_range_sq = prop_range * prop_range
+                    local isendothermic = self.inst.components.heater ~= nil and self.inst.components.heater:IsEndothermic()
 
-								if not isendothermic and (v:HasTag("frozen") or v:HasTag("meltable")) then
-									v:PushEvent("firemelt")
-									v:AddTag("firemelt")
-								end
-							end
+                    for i, v in ipairs(ents) do
+                        if v:IsValid() then
+                            local vx, vy, vz = v.Transform:GetWorldPosition()
+                            local dsq = VecUtil_LengthSq(x - vx, z - vz)
 
-							if self.damages and
-								v.components.propagator ~= nil and
-								-----------------------------------------------
-								dsq < dmg_range_sq and
-								v.components.health ~= nil and
+                            if v ~= self.inst then
+                                if v.components.propagator ~= nil and
+                                    v.components.propagator.acceptsheat and
+                                    not v.components.propagator.pauseheating then
+                                    local percent_heat = math.max(.1, 1 - dsq / prop_range_sq)
+                                    v.components.propagator:AddHeat(self.heatoutput * percent_heat * dt, self.inst)
+                                end
 
-								v.components.health.vulnerabletoheatdamage ~= false then
-								local percent_damage = self.source ~= nil and self.source:HasTag("player") and self.pvp_damagemod or 1
-								v.components.health:DoFireDamage(self.heatoutput * percent_damage * dt)
-							end
-						end
-					end
-				end
-			else
-				if not (self.inst.components.heater ~= nil and self.inst.components.heater:IsEndothermic()) then
-					local prop_range = (self.propagaterange or 3) * TUNING.DSTU.WINTER_FIRE_MOD or 0
-					local ents = TheSim:FindEntities(x, y, z, prop_range, { "frozen", "firemelt" })
-					for i, v in ipairs(ents) do
-						v:PushEvent("stopfiremelt")
-						v:RemoveTag("firemelt")
-					end
-				end
-				if self.currentheat <= 0 then
-					self:StopUpdating()
-				end
-			end
-		else
-			return _OldOnUpdate(self, dt) --Hornet: wait, should I do a return or just call it without the return?
-		end
-	end
+                                if v.components.freezable ~= nil then
+                                    v.components.freezable:AddColdness(-.25 * self.heatoutput * dt)
+                                    if v.components.freezable:IsFrozen() and v.components.freezable.coldness <= 0 then
+                                        --Skip thawing
+                                        v.components.freezable:Unfreeze()
+                                    end
+                                end
+
+                                if not isendothermic and (v:HasTag("frozen") or v:HasTag("meltable")) then
+                                    v:PushEvent("firemelt")
+                                    v:AddTag("firemelt")
+                                end
+                            end
+
+                            if self.damages and
+                                --V2C: DST specific (DSV does not check this)--
+                                --Affects things with health but not burnable--
+                                v.components.propagator ~= nil and
+                                -----------------------------------------------
+                                dsq < dmg_range_sq and
+                                v.components.health ~= nil and
+                                --V2C: vulnerabletoheatdamage isn't used, but we'll keep it in case
+                                --     for MOD support and make nil default to true to save memory.
+                                v.components.health.vulnerabletoheatdamage ~= false then
+                                --V2C: Confirmed that distance scaling was intentionally removed as a design decision
+                                --local percent_damage = math.min(.5, 1 - math.min(1, dsq / dmg_range_sq))
+                                local percent_damage = self.source ~= nil and self.source:HasTag("player") and self.pvp_damagemod or 1
+                                v.components.health:DoFireDamage(self.heatoutput * percent_damage * dt)
+                            end
+                        end
+                    end
+                end
+            else
+                if not (self.inst.components.heater ~= nil and self.inst.components.heater:IsEndothermic()) then
+                    local ents = TheSim:FindEntities(x, y, z, prop_range, TARGET_MELT_MUST_TAGS)
+                    for i, v in ipairs(ents) do
+                        v:PushEvent("stopfiremelt")
+                        v:RemoveTag("firemelt")
+                    end
+                end
+                if self.currentheat == 0 then
+                    self:StopUpdating()
+                end
+            end
+        else
+            _OldOnUpdate(self, dt)
+        end
+    end
 end)
