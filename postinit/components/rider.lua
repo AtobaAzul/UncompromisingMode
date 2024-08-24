43fea3099a4c711b413e22f1695b0f8b508cf337
summerscorcher <88599865+summerscorcher@users.noreply.github.com>
2024-08-25 00:34:34 +0100

    Update rider.lua

--- postinit/components/rider.lua
+++ postinit/components/rider.lua
@@ -2,29 +2,36 @@ local env = env
 GLOBAL.setfenv(1, GLOBAL)
 -----------------------------------------------------------------
 env.AddComponentPostInit("rider", function(self)
-
     local _Mount = self.Mount
 
     function self:Mount(target, instant, ...)
         local ret = _Mount(self, target, instant, ...)
-        if target.components.combat ~= nil then
-            self.inst.components.combat.redirectdamagefn =
+
+        if target.components.combat == nil
+            or not target.components.rideable:TestObedience()
+            or not target.components.rideable:TestRider(self.inst)
+            or self.riding
+            or target.components.rideable == nil
+            or target.components.rideable:IsBeingRidden() then
+            return ret
+        end
+
+        self.inst.components.combat.redirectdamagefn =
             function(inst, attacker, damage, weapon, stimuli)
                 return target:IsValid()
                     and not (target.components.health ~= nil and target.components.health:IsDead())
                     and not (weapon ~= nil and (
                         weapon.components.projectile ~= nil or
-                            weapon.components.complexprojectile ~= nil or
-                            weapon.components.weapon ~= nil and weapon.components.weapon:CanRangedAttack()
-                        ))
+                        weapon.components.complexprojectile ~= nil or
+                        weapon.components.weapon ~= nil and weapon.components.weapon:CanRangedAttack()
+                    ))
                     and stimuli ~= "electric"
                     and stimuli ~= "darkness"
                     and stimuli ~= "beefalo_half_damage"
                     and target
                     or nil
             end
-        end
+
         return ret
     end
-
 end)
