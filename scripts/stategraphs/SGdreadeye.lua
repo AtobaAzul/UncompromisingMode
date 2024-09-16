43857520237578ac913042d41084d4bada5ab808
Maximum101 <90657656+Maximum101@users.noreply.github.com>
2024-09-16 13:09:58 -0400

    Make the new Shadow Creatures use "shadowdominance" instead of "shadowdominant." (#176)
    * Delete postinit/prefabs/skeletonhat.lua
    
    Updating to use "shadowdominance" tag for relevant Shadow Creatures.
    
    * Update uncompromising_shadowcreaturespawner.lua
    
    Changed "shadowdominant" to "shadowdominance" for the "new" 2019 changes.
    
    * Update creepingfear.lua
    
    Changed "shadowdominant" to "shadowdominance" for the "new" 2019 changes.
    
    * Update shadow_goo.lua
    
    Changed "shadowdominant" to "shadowdominance" for the "new" 2019 changes.
    
    * Update SGdreadeye.lua
    
    Changed "shadowdominant" to "shadowdominance" for the "new" 2019 changes.

--- scripts/stategraphs/SGdreadeye.lua
+++ scripts/stategraphs/SGdreadeye.lua
@@ -79,7 +79,7 @@ local function SpikeAoE(inst)
     fx1.Transform:SetPosition(ix, iy, iz)
     inst:DoTaskInTime(1.2, function()
         inst.SoundEmitter:PlaySound("dontstarve/sanity/creature2/attack")
-        local ents = TheSim:FindEntities(ix, iy, iz, 1.2, nil, { "FX", "NOCLICK", "INLIMBO", "shadowdominant" })
+        local ents = TheSim:FindEntities(ix, iy, iz, 1.2, nil, { "FX", "NOCLICK", "INLIMBO", "shadowdominance" })
         for k, v in ipairs(ents) do
             if not target_index[v] and v ~= inst and inst.components.combat:IsValidTarget(v) and v.components.combat and ((v.components.sanity and v.components.sanity:IsInsane()) or v == target) then
                 target_index[v] = true
