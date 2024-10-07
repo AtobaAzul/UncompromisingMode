1ec9e498dbabbf28c002c7c83243f6763b09739e
AtobaAzul <atoba.azul.radical@gmail.com>
2024-10-07 18:12:10 -0300

    Revert "ver"
    This reverts commit c15a226d0e3a4f154ab91e832f3c7085df587ad4.

--- modinfo.lua
+++ modinfo.lua
@@ -6,7 +6,7 @@ if not folder_name:find("workshop-") then
 end
 
 --RELEASE.MAJOR.MINOR.FIX
-local _version = "1.4.21.8"
+local _version = "1.4.21.7"
 description = [[
 󰀔 [ Version: ]] .. _version .. [[ - "Under the Weather Pt.1" ]
 
