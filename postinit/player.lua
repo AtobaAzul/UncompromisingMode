441e0233536b484c2222f0937d400946cdf4c171
AtobaAzul <atoba.azul.radical@gmail.com>
2024-10-07 19:44:08 -0300

    merms no longer travel between shards.
    Apparently that's already a thing

--- postinit/player.lua
+++ postinit/player.lua
@@ -226,7 +226,7 @@ env.AddPlayerPostInit(function(inst)
     inst.OnDespawn = function(inst, migrationdata, ...)
         for k, v in pairs(inst.components.leader.followers) do
             if ((k:HasTag("spider") and not TUNING.DSTU.TREATS_FOR_WEBBER) or
-                    k:HasTag("pig") or k:HasTag("merm") or k:HasTag("raidrat") or
+                    k:HasTag("pig") or k:HasTag("raidrat") or
                     k:HasTag("winky_rat") or k.prefab == "eyeofterror_mini_ally") or
                 k.prefab == "smallbird" or k.prefab == "teenbird" or k.prefab ==
                 "lightflier" then -- exluding things that can't/shouldn't/already do
